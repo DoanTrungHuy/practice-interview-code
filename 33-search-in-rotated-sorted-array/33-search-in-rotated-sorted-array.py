2409db9a499c65f5dacb03f49d3bbd956a3d9a65
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2022-09-25 21:11:14 +0700

    Time: 88 ms (18.35%), Space: 14.4 MB (18.01%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,22 +1,29 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        hashMap = {}
-        n = len(nums)
-        
-        for i in range(n):
-            hashMap[nums[i]] = i
-        
-        nums.sort()
-        
-        l, r = 0, n - 1
-        
-        while l <= r:
-            m = l + (r - l) // 2
-            if nums[m] == target:
-                return hashMap[nums[m]]
-            elif nums[m] < target:
-                l = m + 1
+    def searchMinimum(self, nums, left, right):
+        while left <= right:
+            mid = left + (right - left) // 2
+            if (mid == left or nums[mid - 1] > nums[mid]) and (mid == right or nums[mid + 1] > nums[mid]):
+                return mid
+            elif nums[mid] > nums[right]:
+                left = mid + 1
             else:
-                r = m - 1
-                
+                right = mid - 1
+    
+    def binarySearch(self, nums, left, right, target):
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
         return -1
+    
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        pivot = self.searchMinimum(nums, 0, n - 1)
+        res = self.binarySearch(nums, 0, pivot - 1, target)
+        if res != -1:
+            return res
+        return self.binarySearch(nums, pivot, n - 1, target)
