24030f22ea5de897b02ed7fca0b8025ad8ef4ddd
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2022-10-15 15:32:52 +0700

    Time: 13 ms (50.44%), Space: 7.3 MB (66.29%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -11,12 +11,12 @@ public:
                 return false;
             }
         }
-        for (int x = i, y = j; x >= 0 and y >= 0; --x, --y) {
+        for (int x = i - 1, y = j - 1; x >= 0 and y >= 0; --x, --y) {
             if (nQueens[x][y] == 'Q') {
                 return false;
             }
         }
-        for (int x = i, y = j; x >= 0 and y < col; --x, ++y) {
+        for (int x = i - 1, y = j + 1; x >= 0 and y < col; --x, ++y) {
             if (nQueens[x][y] == 'Q') {
                 return false;
             }
