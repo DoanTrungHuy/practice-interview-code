71aec9d608577089dc58f1b874ca000b6336d6ec
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2022-10-16 23:05:32 +0700

    Time: 212 ms (38.30%), Space: 14 MB (6.54%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        n = len(board)
+        
+        for i in range(n):
+            row = set()
+            for j in range(n):
+                if board[i][j] in row:
+                    return False
+                if '0' <= board[i][j] <= '9':
+                    row.add(board[i][j]) 
+                
+        for j in range(n):
+            col = set()
+            for i in range(n):
+                if board[i][j] in col:
+                    return False
+                if '0' <= board[i][j] <= '9':
+                    col.add(board[i][j])  
+        
+        for i in range(0, n - 2, 3):
+            for j in range(0, n - 2, 3):
+                mat = set()
+                for x in range(i, i + 3):
+                    for y in range(j, j + 3):
+                        if board[x][y] in mat:
+                            return False
+                        if '0' <= board[x][y] <= '9':
+                            mat.add(board[x][y]) 
+                        print(board[x][y], end = ' ')
+                    print()
+        
+        return True
