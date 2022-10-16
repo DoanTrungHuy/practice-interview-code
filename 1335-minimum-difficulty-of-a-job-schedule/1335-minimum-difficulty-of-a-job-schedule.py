2fccb554c0ce48d206375ab49ce43125e5f2c502
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2022-10-16 22:05:22 +0700

    Time: 803 ms (94.03%), Space: 15.1 MB (36.89%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -1,16 +1,27 @@
 class Solution:
-    def minDifficulty(self, A: List[int], d: int) -> int:
-        n = len(A)
-        if n < d: return -1
-
-        @functools.lru_cache(None)
+    def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
+        # d = 2
+        # jobDifficulty: abcd
+        # max(a) + max(b, c, d) | max(a, b) + max(c, d) | max(a, b, c) + d 
+        
+        n = len(jobDifficulty)
+        
+        if n < d:
+            return -1
+        
+        @lru_cache(None)
+        
         def dfs(i, d):
             if d == 1:
-                return max(A[i:])
-            res, maxd = float('inf'), 0
+                return max(jobDifficulty[i:])
+            
+            maxDay = 0
+            minJob = inf
+            
             for j in range(i, n - d + 1):
-                maxd = max(maxd, A[j])
-                res = min(res, maxd + dfs(j + 1, d - 1))
-            return res
+                maxDay = max(maxDay, jobDifficulty[j])
+                minJob = min(minJob, maxDay + dfs(j + 1, d - 1))
+                
+            return minJob
         
         return dfs(0, d)
