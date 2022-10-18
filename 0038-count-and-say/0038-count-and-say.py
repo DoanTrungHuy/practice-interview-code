ead6465727a753f5920bf961c07568f7f12c58a5
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2022-10-18 16:53:19 +0700

    Time: 47 ms (94.07%), Space: 14.4 MB (7.99%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        ans = ['1']
+        
+        for i in range(n - 1):
+            cnt, char = 1, ans[0]
+            tmp = []
+            
+            for j in range(1, len(ans)):
+                if char == ans[j]:
+                    cnt += 1
+                else:
+                    tmp += [str(cnt)]
+                    tmp += [char]
+                    cnt, char = 1, ans[j]
+            
+            tmp += [str(cnt)]
+            tmp += [char]
+            
+            ans = tmp
+                    
+        return ''.join(ans)
