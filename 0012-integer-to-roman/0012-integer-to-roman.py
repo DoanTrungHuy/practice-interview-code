1294540854b1f10145f714ffa1f0653c53a3ab78
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2022-10-20 21:33:51 +0700

    Time: 92 ms (55.73%), Space: 14 MB (35.57%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,16 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        sym = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+        
+        i = 0
+        ans = ''
+        
+        while i < 13 and num > 0:
+            while num >= val[i]:
+                ans += sym[i]
+                num -= val[i]
+            i += 1
+            
+        return ans
+        
