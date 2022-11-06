956712b17fbd5a79404a2d1e8616364c8ec14225
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2022-11-06 16:55:39 +0700

    Time: 357 ms (25.00%), Space: 72.7 MB (25.00%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -1,50 +1,25 @@
 class Solution {
 public:
-    using ll = long long;
-    long long totalCost(vector<int>& costs, int k, int c) {
-        int n = costs.size();
-        priority_queue<ll,vector<ll>,greater<ll>> pq1, pq2;
+    long long totalCost(vector<int>& costs, int k, int candidates) {
+        priority_queue<int, vector<int>, greater<int>> pq1, pq2;
+        const int n = costs.size();
         int i = 0, j = n - 1;
-        while (i < c) {
-            pq1.push(costs[i++]);
-        }
-        if (c > n - c) {
-            c = n - c;
-        }
-        int cnt = 0;
-        while (cnt < c) {
-            pq2.push(costs[j--]);
-            cnt++;
-        }
-        ll res = 0;
+        long long res = 0;
         while (k--) {
-            if (pq1.size() == 0) {
-                res += pq2.top();
-                pq2.pop();
-                if (i <= j) {
-                    pq2.push(costs[j--]);
-                }
+            while (pq1.size() < candidates and i <= j) {
+                pq1.push(costs[i++]);
             }
-            else if (pq2.size() == 0) {
-                res += pq1.top();
+            while (pq2.size() < candidates and i <= j) {
+                pq2.push(costs[j--]);
+            }
+            int a = pq1.size() == 0 ? INT_MAX : pq1.top();
+            int b = pq2.size() == 0 ? INT_MAX : pq2.top();
+            if (a <= b) {
                 pq1.pop();
-                if (i <= j) {
-                    pq1.push(costs[i++]);
-                }
-            }
-            else if (pq1.top() <= pq2.top()) {
-                res += pq1.top();
-                pq1.pop();
-                if (i <= j) {
-                    pq1.push(costs[i++]);
-                }
-            }
-            else {
-                res += pq2.top();
+                res += a;
+            } else {
                 pq2.pop();
-                if (i <= j) {
-                    pq2.push(costs[j--]);
-                }
+                res += b;
             }
         }
         return res;
