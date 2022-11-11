f6470386713cfc1f8f7331229f5a85ecfeec49d8
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2022-11-11 12:35:24 +0700

    Time: 1517 ms (5.01%), Space: 229.2 MB (5.20%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dp[22][1000000];
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            dp[0][i + 1] = nums[i]; 
+        }
+        int m = log2(n) + 1;
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n - (1 << i) + 1; ++j) {
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j + (1 << (i - 1))]);
+            }
+        }
+        vector<int> ans;
+        for (int j = 0; j < n - k + 1; ++j) {
+            int l = j + 1;
+            int r = j + k;
+            int i = log2(r - l + 1);
+            ans.push_back(max(dp[i][l], dp[i][r - (1 << i) + 1]));
+        }
+        return ans;
+    }
+};
