c734443be0abfb800b2c45e40f60c70d952f5b8b
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2022-12-03 14:51:33 +0700

    Time: 124 ms (81.28%), Space: 59.8 MB (93.48%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -1,36 +1,18 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-
 class Solution {
 public:
-    int s = 0;
-    int maxDepth = 1;
-    void dfs(TreeNode *root, int depth) {
-        if (!root) {
-            return;
-        }
-        if (maxDepth <= depth) {
-            if (maxDepth < depth) {
-                s = root->val;
-                maxDepth = depth;
-            } 
-            else if (maxDepth == depth) {
-                s += root->val;
-            }
-        }
-        dfs(root->left, depth + 1), dfs(root->right, depth + 1);
-    }
-    int deepestLeavesSum(TreeNode* root) {
-        dfs(root, 1);
-        return s;
-    }
+   int maxDepth;
+   map <int, int> m;
+   void solve(TreeNode* node, int level = 0){
+      if(!node)return;
+      maxDepth = max(level, maxDepth);
+      m[level] += node->val;
+      solve(node->left, level + 1);
+      solve(node->right, level + 1);
+   }
+   int deepestLeavesSum(TreeNode* root) {
+      maxDepth = 0;
+      m.clear();
+      solve(root);
+      return m[maxDepth];
+   }
 };
