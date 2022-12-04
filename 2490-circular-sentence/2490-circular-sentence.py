6bfdaa2e81c0f6917718a27734ff3ac16a03a5a4
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2022-12-04 11:19:24 +0700

    Time: 60 ms (27.27%), Space: 13.8 MB (100.00%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.py
+++ 2490-circular-sentence/2490-circular-sentence.py
@@ -5,7 +5,7 @@ class Solution:
         
         n = len(s)
         
-        for i in range(n - 1):
+        for i in range(1, n - 1):
             if i and s[i] == ' ' and s[i - 1] != s[i + 1]:
                 return False
             
