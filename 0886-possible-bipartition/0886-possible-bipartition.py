cf0c924f2653e2e7b3412bc3a7da0e8b2f8e1f01
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2022-12-21 17:08:15 +0700

    Time: 1938 ms (30.08%), Space: 20 MB (80.54%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -0,0 +1,38 @@
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        adj = defaultdict(list)
+        visited = defaultdict(bool)
+        color = defaultdict(int)
+        
+        BLUE, RED = 1, 2 
+        
+        for u, v in dislikes:
+            adj[u].append(v)
+            adj[v].append(u)
+            
+        dq = deque()
+        
+        for i in range(1, n + 1):
+            if visited[i]:
+               continue
+            dq.append(i)
+            color[i] = BLUE
+            
+            while dq:
+                u = dq.popleft()
+                
+                if visited[u]:
+                    continue
+                
+                visited[u] = True
+                
+                for v in adj[u]:
+                    if color[v] == color[u]:
+                        return False
+                    if color[u] == RED:
+                        color[v] = BLUE
+                    else:
+                        color[v] = RED
+                    dq.append(v)
+                    
+        return True
