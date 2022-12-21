1425f1467300a7bfe1abd120fe6b5e670d750d48
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2022-12-21 17:53:19 +0700

    Time: 1936 ms (30.29%), Space: 21.9 MB (35.09%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -1,39 +1,24 @@
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
-        adj = defaultdict(list)
-        visited = defaultdict(bool)
-        color = defaultdict(int)
-        
-        BLUE, RED = 1, 2 
+        adj = [[] for _ in range(n + 1)]
         
         for u, v in dislikes:
             adj[u].append(v)
             adj[v].append(u)
             
-        dq = deque()
+        color = [0 for _ in range(n + 1)]
         
-        for i in range(1, n + 1):
-            if visited[i]:
-               continue
+        def dfs(u, tincture):
+            if color[u] != 0:
+                return color[u] == tincture
+            color[u] = tincture
+            for v in adj[u]:
+                if not dfs(v, -tincture):
+                    return False
+            return True
             
-            dq.append(i)
-            color[i] = BLUE
-            
-            while dq:
-                u = dq.popleft()
-                
-                if visited[u]:
-                    continue
-                
-                visited[u] = True
-                
-                for v in adj[u]:
-                    if color[v] == color[u]:
-                        return False
-                    if color[u] == RED:
-                        color[v] = BLUE
-                    else:
-                        color[v] = RED
-                    dq.append(v)
-                    
+        for u in range(1, n + 1):
+            if not color[u] and not dfs(u, 1):
+                return False
+        
         return True
