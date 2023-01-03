1c76e3efd376b37196d30c071c44a050d241e112
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-01-04 01:23:18 +0700

    Time: 7807 ms (6.66%), Space: 13.9 MB (44.24%) - LeetHub

---
+++ 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minimumTimeRequired(self, jobs: List[int], k: int) -> int:
+        n = len(jobs)
+        flags = [0] * k
+        self.minDiff = sys.maxsize
+        
+        def backTracking(i):
+            if i == n:
+                self.minDiff = min(self.minDiff, max(flags))
+                return
+            
+            seen = set()
+            
+            for j in range(k):
+                if flags[j] in seen:
+                    continue
+                if flags[j] + jobs[i] >= self.minDiff:
+                    continue
+                    
+                seen.add(flags[j])
+                flags[j] += jobs[i]
+                backTracking(i + 1)
+                flags[j] -= jobs[i]
+        
+        backTracking(0)
+        return self.minDiff
