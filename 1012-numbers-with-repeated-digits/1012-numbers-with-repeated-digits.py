f3b25a485465a0b9683ae444ef2ad37558bb93df
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-01-04 05:57:19 +0700

    Time: 3630 ms (6.56%), Space: 93.4 MB (8.20%) - LeetHub

--- 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.py
+++ 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.py
@@ -4,25 +4,20 @@ class Solution:
         sz = len(nums)
         
         @cache
-        def dp(i, tight, mask):
+        def dp(i, tight, rep, mask):
             if i == sz:
-                return mask != 0
+                return 1 if rep else 0
             
             ans = 0
             
             maxi = nums[i] if tight else 9
             
-            for d in range(maxi  + 1):
-                if mask & (1 << d):
-                    continue
-                    
+            for d in range(maxi + 1):    
+                newTight = tight and nums[i] == d
                 newMask = mask if mask == 0 and d == 0 else (mask | (1 << d))
-                
-                if tight:
-                    ans += dp(i + 1, nums[i] == d, newMask)
-                else:
-                    ans += dp(i + 1, False, newMask)
+                newRep = rep or (mask & (1 << d))
+                ans += dp(i + 1, newTight, newRep, newMask)
                     
             return ans
             
-        return n - dp(0, True, 0)
+        return dp(0, True, False, 0)
