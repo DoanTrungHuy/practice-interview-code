cc72a82467543c1b7ce391d1336227fd985d8323
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-01-04 16:27:21 +0700

    Time: 33 ms (22.61%), Space: 6.1 MB (80.00%) - LeetHub

--- 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.cpp
+++ 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.cpp
@@ -1,60 +1,31 @@
 class Solution {
 public:
+    int dp[11][2][2][1 << 10];
+    int sz;
+    string nums;
+    
+    int dfs(int i, bool tight, bool hasRep, int mask) {
+        if (i == sz) {
+            return hasRep;
+        }
+        if (dp[i][tight][hasRep][mask] != -1) {
+            return dp[i][tight][hasRep][mask];
+        }
+        int limit = tight ? nums[i] - '0' : 9;
+        int ans = 0;
+        for (int d = 0; d <= limit; ++d) {
+            bool newTight = tight and d == nums[i] - '0';
+            bool newRep = hasRep or mask & (1 << d); 
+            int newMask = d == 0 and mask == 0 ? mask : mask | (1 << d);
+            ans += dfs(i + 1, newTight, newRep, newMask);
+        }
+        return dp[i][tight][hasRep][mask] = ans;
+    }
+    
     int numDupDigitsAtMostN(int n) {
-        int dp[11][1 << 10][3];
-        memset(dp, 0, sizeof(dp));
-        int total = n;
-        vector<int> nums;
-        while (n > 0) {
-            nums.push_back(n % 10);
-            n /= 10;
-        }
-        reverse(nums.begin(), nums.end());
-        for (int d = 1; d <= 9; ++d) {
-            int states = 0;
-            if (d > nums[0]) {
-                states = 2;
-            }
-            else if (d == nums[0]) {
-                states = 1;
-            }
-            dp[1][1 << d][states]++;
-        }
-        int sz = nums.size();
-        for (int i = 1; i < sz; ++i) {
-            for (int mask = 1; mask < (1 << 10); ++mask) {
-                for (int states = 0; states <= 2; ++states) {
-                    if (dp[i][mask][states] == 0) {
-                        continue;
-                    }
-                    for (int d = 0; d <= 9; ++d) {
-                        if (mask & (1 << d)) {
-                            continue;
-                        }
-                        int newStates = states;
-                        int newMask = (mask | (1 << d));
-                        if (states == 1) {
-                            if (d > nums[i]) {
-                                newStates = 2;
-                            }
-                            else if (d < nums[i]) {
-                                newStates = 0;
-                            }
-                        }
-                        dp[i + 1][newMask][newStates] += dp[i][mask][states];
-                    }
-                }
-            }
-        }
-        int negAns = 0;
-        for (int i = 1; i <= sz; ++i) {
-            for (int mask = 1; mask < (1 << 10); ++mask) {
-                int szStates = (i == sz) ? 1 : 2;
-                for (int states = 0; states <= szStates; ++states) {
-                    negAns += dp[i][mask][states];
-                }
-            } 
-        }
-        return total - negAns;
+        memset(dp, -1, sizeof(dp));
+        nums = to_string(n);
+        sz = nums.size();
+        return dfs(0, true, false, 0);
     }
 };
