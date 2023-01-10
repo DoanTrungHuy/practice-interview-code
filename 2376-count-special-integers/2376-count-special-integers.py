115952e609d672676e03101b53db2da2e7da8458
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-01-10 15:59:44 +0700

    Time: 341 ms (32.20%), Space: 19.8 MB (23.73%) - LeetHub

--- 2376-count-special-integers/2376-count-special-integers.py
+++ 2376-count-special-integers/2376-count-special-integers.py
@@ -1,40 +1,24 @@
 class Solution:
     def countSpecialNumbers(self, n: int) -> int:
-        dp = [[[0 for _ in range(3)] for _ in range(1 << 10)] for _ in range(11)]
-        nums = list(map(int, list(str(n))))
-        sz = len(nums)
+        nums = list(map(int, str(n)))
+        sizeDigit = len(nums)
         
-        for d in range(1, 10):
-            states = 0
-            if d == nums[0]:
-                states = 1
-            elif d > nums[0]:
-                states = 2
-            dp[1][1 << d][states] += 1
+        @cache
+        def dp(i, tight, mask):
+            if i == sizeDigit:
+                return 1 if mask else 0
             
-        for i in range(1, sz):
-            for mask in range(1, (1 << 10)):
-                for states in range(3):
-                    if dp[i][mask][states] == 0:
-                        continue
-                    for d in range(10):
-                        if mask & (1 << d) > 0:
-                            continue
-                        newMask = mask | (1 << d)
-                        newStates = states
-                        if states == 1:
-                            if d < nums[i]:
-                                newStates = 0
-                            elif d > nums[i]:
-                                newStates = 2
-                        dp[i + 1][newMask][newStates] += dp[i][mask][states]
+            limit = nums[i] if tight else 9
+            limit += 1
+            ans = 0
             
-        ans = 0
-        
-        for i in range(1, sz + 1):
-            for mask in range(1, (1 << 10)):
-                szStates = 1 if i == sz else 2
-                for states in range(szStates + 1):
-                    ans += dp[i][mask][states]
-                    
-        return ans
+            for d in range(limit):
+                if mask & (1 << d):
+                    continue
+                newTight = tight and d == nums[i]
+                newMask = mask if d == 0 and mask == 0 else mask | (1 << d)
+                ans += dp(i + 1, newTight, newMask)
+                
+            return ans
+            
+        return dp(0, True, 0)
