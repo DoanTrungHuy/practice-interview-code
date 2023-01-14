292e1afbbd0c471ddf9cac5980dd15a5a6ff201b
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-01-14 16:08:21 +0700

    Update 0001-two-sum.cpp

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,10 +2,6 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> um;
-        // j < i
-        // a[i] + a[j] = target
-        // a[i] = target - a[j] -> Giu chi muc j 
-        // -> return [um[a[i]], i] -> [j, i]
         int n = nums.size();
         for (int i = 0; i < n; ++i) {
             if (um.find(nums[i]) != um.end()) {
@@ -14,7 +10,5 @@ public:
             um[target - nums[i]] = i;
         }
         return {-1, -1};
-        // um[target - a[j]] = j
-        // um[7] = 0
     }
 };
