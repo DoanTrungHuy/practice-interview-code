aa2d46fa4ec17b00c31d26b476b33fb39eb72222
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-01-16 19:57:20 +0700

    Time: 2891 ms (66.67%), Space: 35.5 MB (22.22%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.py
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.py
@@ -1,10 +1,15 @@
-import numpy as np
-
 class Solution:
     def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
-        nums = np.zeros((n, n), dtype = np.int16)
+        ans = [[0 for _ in range(n)] for _ in range(n)]
         
         for x1, y1, x2, y2 in queries:
-            nums[x1:x2 + 1, y1:y2 + 1] += 1
-            
-        return nums
+            for r in range(x1, x2 + 1):
+                ans[r][y1] += 1
+                if y2 + 1 < n:
+                    ans[r][y2 + 1] -= 1
+                    
+        for r in range(n):
+            for c in range(1, n):
+                ans[r][c] += ans[r][c - 1]
+                
+        return ans
