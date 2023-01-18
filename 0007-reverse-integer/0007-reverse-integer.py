ef3ae5ddaa5f876fb0af5fd004a2fe8095a461df
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-01-18 17:12:57 +0700

    Time: 32 ms (87.73%), Space: 13.8 MB (96.94%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        front = [1, -1][x < 0]
+        
+        num = front * int(str(abs(x))[::-1])
+        
+        return num if -2**31 <= num <= 2**31 - 1 else 0
