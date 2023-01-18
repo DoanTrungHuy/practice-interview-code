dce43750dd3dd55efb9a32eb9c0521997f7709ea
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-01-18 17:45:04 +0700

    Time: 3035 ms (57.56%), Space: 81.6 MB (72.99%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        adj = list()
+        n = len(points)
+        
+        for i in range(n):
+            for j in range(i + 1, n):
+                x1, y1 = points[i]
+                x2, y2 = points[j]
+                dist = abs(x1 - x2) + abs(y1 - y2)
+                adj.append((dist, i, j))
+                
+        adj.sort()
+        self.parent = list(range(n))
+        
+        def find(u: int) -> int:
+            if u != self.parent[u]:
+                self.parent[u] = find(self.parent[u])
+            return self.parent[u]
+        
+        def union(u: int, v: int) -> bool:
+            u = find(u)
+            v = find(v)
+            if u != v:
+                self.parent[u] = v
+                return True
+            return False
+        
+        m = len(adj)
+        ans = 0
+        
+        for i in range(m):
+            dist, u, v = adj[i]
+            if union(u, v):
+                ans += dist
+                
+        return ans
