9f98cdbd3c5bec0d8909afd49c4929485f132a40
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-01-18 10:29:00 +0700

    Time: 917 ms (54.36%), Space: 82.2 MB (31.71%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        n, m = len(grid), len(grid[0])
+        dx = [-1, 1, 0, 0]
+        dy = [0, 0, -1, 1]
+        
+        def dfs(x: int, y: int) -> int:
+            if not (0 <= x < n and 0 <= y < m and grid[x][y] == 1):
+                return 0
+            grid[x][y] = 0
+            res = 1
+            for i in range(4):
+                res += dfs(x + dx[i], y + dy[i])
+            return res
+        
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j]:
+                    if i == 0 or j == 0 or i == n - 1 or j == m - 1:
+                        dfs(i, j)
+                    
+        cnt = 0
+        
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j]:
+                    cnt += dfs(i, j)
+                    
+        return cnt
