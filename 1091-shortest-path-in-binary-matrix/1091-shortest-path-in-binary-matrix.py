1e6eeca4fd5cc47899979d817899e39b29b435bc
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-01-19 23:24:11 +0700

    Time: 499 ms (99.88%), Space: 14.1 MB (93.79%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,7 +1,7 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         DIR = []
-        n, m = len(grid), len(grid[0])
+        n = len(grid)
         
         for x in [-1, 0, 1]:
             for y in [-1, 0, 1]:
@@ -18,14 +18,14 @@ class Solution:
         while dq:
             x, y, d = dq.popleft()
             
-            if x == n - 1 and y == m - 1:
+            if x == n - 1 and y == n - 1:
                 return d
             
             for dx, dy in DIR:
                 nx = x + dx
                 ny = y + dy
                 
-                if 0 <= nx < n and 0 <= ny < m and grid[nx][ny] == 0:
+                if 0 <= nx < n and 0 <= ny < n and grid[nx][ny] == 0:
                     grid[nx][ny] = 1
                     dq.append((nx, ny, d + 1))
 
