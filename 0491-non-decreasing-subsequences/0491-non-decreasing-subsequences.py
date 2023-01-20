f98ee3a96f64ff4ad7dce8ca9717379e6e84054d
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-01-20 14:42:11 +0700

    Time: 730 ms (13.13%), Space: 22.5 MB (28.91%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findSubsequences(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        ans = set()
+        
+        for mask in range(1 << n):
+            tmp = []
+            flag = True
+            for i in range(n):
+                bit = n - i - 1
+                if (mask >> bit) & 1:
+                    if not tmp:
+                        tmp.append(nums[i])
+                    elif nums[i] >= tmp[-1]:
+                        tmp.append(nums[i])
+                    else:
+                        flag = False
+                        break
+            if len(tmp) >= 2 and flag:
+                ans.add(tuple(tmp))
+                
+        return ans  
