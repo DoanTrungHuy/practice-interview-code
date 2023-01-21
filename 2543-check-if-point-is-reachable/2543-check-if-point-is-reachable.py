d9232a370ebc122329faa942edfa9e7aa8f01d57
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-01-21 23:35:16 +0700

    Time: 57 ms (33.33%), Space: 13.7 MB (100.00%) - LeetHub

--- 2543-check-if-point-is-reachable/2543-check-if-point-is-reachable.py
+++ 2543-check-if-point-is-reachable/2543-check-if-point-is-reachable.py
@@ -1,7 +1,12 @@
 class Solution:
-    def isReachable(self, x: int, y: int) -> bool:
-        while x % 2 == 0:
-            x //= 2
-        while y % 2 == 0:
-            y //= 2
-        return gcd(x, y) == 1
+    def isReachable(self, targetX: int, targetY: int) -> bool:
+        tx, ty = targetX, targetY
+        while tx & 1 == 0:
+            tx >>= 1
+        while ty & 1 == 0:
+            ty >>= 1
+        g = math.gcd(tx, ty)
+        if g == 1:
+            return True
+        else:
+            return False
