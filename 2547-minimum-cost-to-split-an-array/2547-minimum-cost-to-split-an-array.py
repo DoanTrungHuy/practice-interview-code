50c5b1de931d3bc2852c9434f22800465f503b13
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-01-22 20:11:31 +0700

    Time: 5692 ms (66.67%), Space: 14.2 MB (66.67%) - LeetHub

---
+++ 2547-minimum-cost-to-split-an-array/2547-minimum-cost-to-split-an-array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minCost(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        dp = [inf for _ in range(n + 2)]
+        dp[0] = 0
+        
+        for i in range(n):
+            cnt = [0 for _ in range(n)]
+            trimmed = 0
+            for j in range(i + 1, n + 1):
+                cnt[nums[j - 1]] += 1
+                if cnt[nums[j - 1]] == 2:
+                    trimmed += 2
+                elif cnt[nums[j - 1]] > 2:
+                    trimmed += 1
+                dp[j] = min(dp[j], dp[i] + k + trimmed)
+                
+        return dp[n]
