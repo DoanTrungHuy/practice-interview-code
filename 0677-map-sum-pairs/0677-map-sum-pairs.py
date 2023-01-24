bbff81098823328013a57e45408090bfd4a7103c
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-01-25 02:29:51 +0700

    Time: 44 ms (38.96%), Space: 14 MB (69.76%) - LeetHub

--- 0677-map-sum-pairs/0677-map-sum-pairs.py
+++ 0677-map-sum-pairs/0677-map-sum-pairs.py
@@ -1,17 +1,18 @@
 class Node:
     def __init__(self):
-        self.val = 0
+        self.total = 0
         self.children = defaultdict(Node)
         
 class Trie:
     def __init__(self):
         self.root = Node()
     
-    def insert(self, word: str, val: int) -> None:
+    def insert(self, word: str, diff: int) -> None:
         root = self.root
         for char in word:
+            root.total += diff
             root = root.children[char]
-        root.val = val
+        root.total += diff
         
     def find(self, prefix: str) -> int:
         root = self.root
@@ -19,25 +20,19 @@ class Trie:
             if char not in root.children:
                 return 0
             root = root.children[char]
-            
-        self.res = 0
-            
-        def dfs(root: Optional[Node]) -> None:
-            self.res += root.val
-            for suffixNode in root.children.values():
-                dfs(suffixNode)
         
-        dfs(root)
-        
-        return self.res
+        return root.total
     
 class MapSum:
 
     def __init__(self):
         self.trie = Trie()
+        self.umap = defaultdict(int)
 
     def insert(self, key: str, val: int) -> None:
-        self.trie.insert(key, val)
+        diff = val - self.umap[key]
+        self.trie.insert(key, diff)
+        self.umap[key] = val
 
     def sum(self, prefix: str) -> int:
         return self.trie.find(prefix)
