b675b05c4a0427136d6a11b97de25b6920fe2b0b
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-01-25 19:42:14 +0700

    Time: 1037 ms (46.09%), Space: 30.5 MB (66.65%) - LeetHub

--- 2541-minimum-operations-to-make-array-equal-ii/2541-minimum-operations-to-make-array-equal-ii.py
+++ 2541-minimum-operations-to-make-array-equal-ii/2541-minimum-operations-to-make-array-equal-ii.py
@@ -1,22 +1,22 @@
 class Solution:
     def minOperations(self, nums1: List[int], nums2: List[int], k: int) -> int:
-        if nums1 == nums2:
-            return 0
-
-        n = len(nums1)
-        
-        diff = [nums1[i] - nums2[i] for i in range(n)]
+        if k == 0:
+            if nums1 == nums2:
+                return 0
+            else:
+                return -1
             
-        if sum(diff) != 0:
-            return -1
+        up, down = 0, 0
         
-        ans = 0
-        
-        for num in diff:
-            if num > 0:
-                ans += num
+        for num1, num2 in zip(nums1, nums2):
+            diff = abs(num1 - num2)
             
-        if k != 0 and ans % k == 0:
-            return ans // k
-        
-        return -1
+            if diff % k != 0:
+                return -1
+            
+            if num1 > num2:
+                up += diff // k
+            else:
+                down += diff // k
+            
+        return up if up == down else -1
