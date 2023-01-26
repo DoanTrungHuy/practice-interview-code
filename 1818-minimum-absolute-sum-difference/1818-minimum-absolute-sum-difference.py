12065bd6ce51408c64f50b9fcf46fab358bbeb74
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-01-26 23:43:18 +0700

    Time: 1672 ms (40.40%), Space: 29.9 MB (29.06%) - LeetHub

---
+++ 1818-minimum-absolute-sum-difference/1818-minimum-absolute-sum-difference.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minAbsoluteSumDiff(self, nums1: List[int], nums2: List[int]) -> int:
+        if nums1 == nums2:
+            return 0
+        
+        target = []
+        total = 0
+        
+        for num1, num2 in zip(nums1, nums2):
+            diff = abs(num1 - num2)
+            total += diff
+            target.append(diff)
+        
+        nums1 = sorted(nums1)
+        ans = total
+        n = len(nums1)
+        
+        for num, diff in zip(nums2, target):
+            idx = bisect_left(nums1, num)
+            if idx > 0:
+                ans = min(ans, total - diff + abs(nums1[idx - 1] - num))
+            if idx < n:
+                ans = min(ans, total - diff + abs(nums1[idx] - num))
+        
+        return ans % (10**9 + 7)
