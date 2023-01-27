2120fe8145a6d62ba0f70b346c12a22812e61eba
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-01-27 19:43:37 +0700

    Time: 1671 ms (14.72%), Space: 40.9 MB (6.78%) - LeetHub

---
+++ 0472-concatenated-words/0472-concatenated-words.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__(self):
+        self.char = ''
+        self.isEndWord = False
+        self.children = defaultdict(Node)
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+        
+    def insert(self, word: str) -> None:
+        root = self.root
+        
+        for char in word:
+            root = root.children[char]
+        
+        root.isEndWord = True
+        
+    def find(self, word: str) -> int:
+        root = self.root
+        
+        for char in word:
+            if char not in root.children:
+                return False
+            root = root.children[char]
+            
+        return root.isEndWord
+        
+
+class Solution:
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        trie = Trie()
+        
+        for word in words:
+            trie.insert(word)
+        
+        def dfs(i, n, word, cnt):
+            root = trie.root
+            for j in range(i, n):
+                root = root.children[word[j]]
+                if root.isEndWord:
+                    if j == n - 1: 
+                        return cnt >= 1
+                    elif dfs(j + 1, n, word, cnt + 1):
+                        return True
+            return False
+        
+        ans = []
+        
+        for word in words:
+            if dfs(0, len(word), word, 0):
+                ans.append(word)
+                
+        return ans
