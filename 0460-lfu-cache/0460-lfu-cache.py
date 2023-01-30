bf7fc9f1e5976de5b560d48711d9afe53239c465
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-01-30 11:38:50 +0700

    Time: 1363 ms (29.78%), Space: 78.1 MB (62.76%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.py
+++ 0460-lfu-cache/0460-lfu-cache.py
@@ -1,90 +1,45 @@
-import collections
-
 class Node:
-    def __init__(self, key, val):
+    def __init__(self, key, val, cnt):
         self.key = key
         self.val = val
-        self.freq = 1
-        self.prev = self.next = None
+        self.cnt = cnt
 
-class DLinkedList:
-    def __init__(self):
-        self._sentinel = Node(None, None)
-        self._sentinel.next = self._sentinel.prev = self._sentinel
-        self._size = 0
-    
-    def __len__(self):
-        return self._size
-    
-    def append(self, node):
-        node.next = self._sentinel.next
-        node.prev = self._sentinel
-        node.next.prev = node
-        self._sentinel.next = node
-        self._size += 1
-    
-    def pop(self, node=None):
-        if self._size == 0:
-            return
-        
-        if not node:
-            node = self._sentinel.prev
-
-        node.prev.next = node.next
-        node.next.prev = node.prev
-        self._size -= 1
-        
-        return node
-        
 class LFUCache:
-    def __init__(self, capacity):
-        self._size = 0
-        self._capacity = capacity
-        
-        self._node = dict()
-        self._freq = collections.defaultdict(DLinkedList)
-        self._minfreq = 0
-        
-        
-    def _update(self, node):
-        freq = node.freq
-        
-        self._freq[freq].pop(node)
-        if self._minfreq == freq and not self._freq[freq]:
-            self._minfreq += 1
-        
-        node.freq += 1
-        freq = node.freq
-        self._freq[freq].append(node)
-    
-    def get(self, key):
-        if key not in self._node:
-            return -1
-        
-        node = self._node[key]
-        self._update(node)
-        return node.val
+    def __init__(self, capacity: int):
+        self.countOfLL = defaultdict(deque)
+        self.keyOfNode = defaultdict(Node)
+        self.capacity = capacity
+        self.minFreq = 0
 
-    def put(self, key, value):
-        if self._capacity == 0:
+    def get(self, key: int) -> int:
+        if key not in self.keyOfNode:
+            return -1
+
+        node = self.keyOfNode[key]
+        self.countOfLL[node.cnt].remove(key)
+
+        node.cnt += 1
+        self.countOfLL[node.cnt].appendleft(key)
+
+        if not self.countOfLL[self.minFreq]:
+            self.minFreq += 1
+
+        return node.val
+        
+
+    def put(self, key: int, value: int) -> None:
+        if self.capacity == 0:
             return
         
-        if key in self._node:
-            node = self._node[key]
-            self._update(node)
-            node.val = value
+        if key in self.keyOfNode:
+            self.keyOfNode[key].val = value
+            self.get(key)
         else:
-            if self._size == self._capacity:
-                node = self._freq[self._minfreq].pop()
-                del self._node[node.key]
-                self._size -= 1
-                
-            node = Node(key, value)
-            self._node[key] = node
-            self._freq[1].append(node)
-            self._minfreq = 1
-            self._size += 1
-        
+            if len(self.keyOfNode) == self.capacity:
+                del self.keyOfNode[self.countOfLL[self.minFreq].pop()]
+            self.minFreq = 1
+            self.keyOfNode[key] = Node(key, value, self.minFreq)
+            self.countOfLL[self.minFreq].appendleft(key)
 
 
 # Your LFUCache object will be instantiated and called as such:
