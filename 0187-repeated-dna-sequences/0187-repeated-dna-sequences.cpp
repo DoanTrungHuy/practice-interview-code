e1757be7274411c090e213f9490957efbf53e0ae
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-02-06 02:54:47 +0700

    Time: 50 ms (93.94%), Space: 17.6 MB (87.16%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -1,10 +1,10 @@
+using ll = long long;
+const ll base = 4;
+const ll mod = 1e9 + 7;
+const ll limit = 9;
+
 class Solution {
 public:
-    using ll = long long;
-    const ll base = 4;
-    const ll mod = 1e9 + 7;
-    const ll limit = 9;
-    
     vector<string> findRepeatedDnaSequences(string s) {
         int n = (int)s.size();
         if (n < 10) {
@@ -21,7 +21,7 @@ public:
             h[i] = (h[i - 1]*base + codes[s[i - 1]]) % mod;
             p[i] = (p[i - 1]*base) % mod;
         }
-        unordered_map<ll, int> um;
+        unordered_map<int, int> um;
         vector<string> ans;
         for (int i = 1; i <= n - limit; ++i) {
             int l = i;
