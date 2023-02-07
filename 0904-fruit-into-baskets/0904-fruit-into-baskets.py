0b07546430f9bc47a17c07e3bb420fbe16b6eec3
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-02-07 16:27:00 +0700

    Time: 1008 ms (61.32%), Space: 20.1 MB (80.52%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,17 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        h = defaultdict(int)
+        j, n = 0, len(fruits)
+        ans = 0
+        
+        for i in range(n):
+            h[fruits[i]] += 1
+            while len(h) > 2:
+                h[fruits[j]] -= 1
+                if h[fruits[j]] == 0:
+                    del h[fruits[j]]
+                j += 1
+                
+            ans = max(ans, i - j + 1)
+            
+        return ans
