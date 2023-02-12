baddecafdb45c943e2486091f7d08f0f187ab282
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-02-12 11:44:46 +0700

    Time: 9381 ms (5.55%), Space: 79.8 MB (5.56%) - LeetHub

--- 2564-substring-xor-queries/2564-substring-xor-queries.py
+++ 2564-substring-xor-queries/2564-substring-xor-queries.py
@@ -6,9 +6,7 @@ class Solution:
             val = a ^ b
             val = bin(val)[2:]
             if val in s:
-                i = s.index(val)
-                k = len(val)
-                ans.append([i , i + k - 1])
+                ans.append([s.index(val) , s.index(val) + len(val) - 1])
             else:
                 ans.append([-1,-1])
                 
