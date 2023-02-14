96d4e7066b88e05d370ea4178331b8f90139f0f5
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-02-14 07:20:36 +0700

    Time: 5 ms (72.12%), Space: 10.2 MB (60.54%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        // Time: O(N * M) -> O(max(N, M))
-        
-        // Dua vao khoang gia tri
-        // 0 <= nums1[i], nums2[i] <= 1000
-        
         int cnt[1001] = {0};
         bool check[1001] = {false};
         
