7620fb08548e1bbff211a5a339f0a3036fbb073c
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-02-14 07:19:30 +0700

    Time: 140 ms (49.59%), Space: 69.5 MB (25.40%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        // 1s -> (10^6 -> 10^7)
-        // N = nums.length
-        // Time: O(N)
-        
         unordered_set<int> hashSet;
         
         for (int num : nums) {
