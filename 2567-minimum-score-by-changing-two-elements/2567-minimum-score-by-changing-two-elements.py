0cd0070ab36cc4a04a7968172b078380190d9656
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-02-18 23:34:31 +0700

    Time: 348 ms (33.33%), Space: 26.6 MB (100.00%) - LeetHub

--- 2567-minimum-score-by-changing-two-elements/2567-minimum-score-by-changing-two-elements.py
+++ 2567-minimum-score-by-changing-two-elements/2567-minimum-score-by-changing-two-elements.py
@@ -1,5 +1,4 @@
 class Solution:
     def minimizeSum(self, nums: List[int]) -> int:
         nums.sort()
-        n = len(nums)
-        return min(abs(nums[0] - nums[n - 3]), abs(nums[1] - nums[n - 2]), abs(nums[2] - nums[n - 1]))
+        return min(abs(nums[0] - nums[-3]), abs(nums[1] - nums[-2]), abs(nums[2] - nums[-1]))
