219afac68fea0d9aa0fce4724155d6afb4e4996c
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-02-25 09:47:53 +0700

    Time: 115 ms (84.60%), Space: 13.9 MB (51.34%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -8,7 +8,7 @@ class Solution:
         for i in range(n):
             d[s[i]] += 1
             maxFreq = max(maxFreq, d[s[i]])
-            if i - j + 1 - maxFreq > k:
+            while i - j + 1 - maxFreq > k:
                 d[s[j]] -= 1
                 j += 1
             maxLeng = max(maxLeng, i - j + 1)
