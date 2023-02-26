ec7ccf1460f83296b4af89fc233bf2f1c1f9190b
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-02-26 11:08:09 +0700

    Time: 661 ms (100.00%), Space: 28.7 MB (11.11%) - LeetHub

--- 2576-find-the-maximum-number-of-marked-indices/2576-find-the-maximum-number-of-marked-indices.py
+++ 2576-find-the-maximum-number-of-marked-indices/2576-find-the-maximum-number-of-marked-indices.py
@@ -1,24 +1,12 @@
 class Solution:
     def maxNumOfMarkedIndices(self, nums: List[int]) -> int:
-        h1, h2 = [], []
-        
+        m = len(nums) // 2
+        i, j = 0, m
+        res = 0
         nums.sort()
-        n = len(nums)
-        
-        for i in range(n // 2):
-            heappush(h1, -nums[i])
-        
-        for i in range(n // 2, n):
-            heappush(h2, -nums[i])
-            
-        ans = 0
-        
-        while h1 and h2:
-            while h1 and -h1[0] * 2 > -h2[0]:
-                heappop(h1)
-            if h1:
-                ans += 2
-                heappop(h1)
-            heappop(h2)
-            
-        return ans
+        while i < m and j < len(nums):
+            if nums[j] >= nums[i] * 2:
+                res += 2
+                i += 1
+            j += 1
+        return res
