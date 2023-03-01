7a3812ae2b693bef46b33ddea9c7e506bed9be20
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-03-01 10:20:45 +0700

    Time: 2459 ms (33.98%), Space: 5.9 MB (81.19%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum = 0;
+        for (int i = 1; i < num; ++i) {
+            if (num % i == 0) {
+                sum += i;
+                if (sum > num) {
+                    return false;
+                }
+            }
+        }
+        return sum == num;
+    }
+};
