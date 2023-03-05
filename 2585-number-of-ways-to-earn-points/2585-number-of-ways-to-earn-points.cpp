92bba9b1b3c76e1be3f5a1aa9b474f52cea7ba42
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-03-05 11:37:55 +0700

    Time: 365 ms (100.00%), Space: 8 MB (33.33%) - LeetHub

--- 2585-number-of-ways-to-earn-points/2585-number-of-ways-to-earn-points.cpp
+++ 2585-number-of-ways-to-earn-points/2585-number-of-ways-to-earn-points.cpp
@@ -1,30 +1,18 @@
-const int mod = 1e9 + 7;
-
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int n;
-    
-    int dfs(int i, int marks, int target, vector<vector<int>>& types) {
-        if (target == marks) {
-            return 1;
-        }
-        if (marks > target or i >= n) {
-            return 0;
-        }
-        if (dp[i][marks] != -1) {
-            return dp[i][marks];
-        }
-        long long cnt = 0;
-        for (int j = 0; j <= types[i][0]; ++j) {
-            cnt += dfs(i + 1, j * types[i][1] + marks, target, types);
-            cnt %= mod;
-        }
-        return dp[i][marks] = cnt;
-    }
     int waysToReachTarget(int target, vector<vector<int>>& types) {
-        n = types.size();
-        dp.resize(n, vector<int>(target + 1, -1));
-        return dfs(0, 0, target, types);
+        const int mod = 1e9 + 7;
+        int n = types.size();
+        vector<int> dp(target + 1);
+        dp[0] = 1;
+        for (int i = 0; i < n; ++i) {
+            for (int j = target; j >= 1; --j) {
+                for (int k = 1; k <= types[i][0] and j - k*types[i][1] >= 0; ++k) {
+                    dp[j] += dp[j - k*types[i][1]];
+                    dp[j] %= mod;
+                }
+            }
+        }
+        return dp[target];
     }
 };
