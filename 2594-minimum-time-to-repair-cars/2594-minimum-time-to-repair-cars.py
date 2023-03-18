c9c2da8bb3c1c6570a17468d5a3f9715530d13f0
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-03-18 23:05:15 +0700

    Time: 2986 ms (100.00%), Space: 17.8 MB (100.00%) - LeetHub

---
+++ 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.py
@@ -0,0 +1,21 @@
+class Solution:
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        ranks.sort()
+        
+        left, right = 1, pow(10, 18)
+        ans = 0
+        
+        while left <= right:
+            mid = (left + right) // 2
+            totalCars = 0
+            for rank in ranks:
+                cntCars = int(sqrt(mid // rank))
+                totalCars += cntCars
+                
+            if totalCars >= cars:
+                ans = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return ans
