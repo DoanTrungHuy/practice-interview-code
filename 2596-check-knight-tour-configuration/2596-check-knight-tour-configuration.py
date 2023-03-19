eb011c82cc491121f32e3bbd9d3249b8e0c9b95b
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-03-19 11:01:37 +0700

    Time: 78 ms (100.00%), Space: 13.9 MB (100.00%) - LeetHub

---
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkValidGrid(self, grid: List[List[int]]) -> bool:
+        DIR = [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1)]
+        n = len(grid)
+        self.check = False
+        oo = 1000
+        
+        def dfs(x, y, step, k):
+            for dx, dy in DIR:
+                nx = x + dx
+                ny = y + dy
+                if 0 <= nx < n and 0 <= ny < n and grid[nx][ny] == step:
+                    grid[nx][ny] = oo
+                    dfs(nx, ny, step + 1, k + 1)
+                    break
+            
+        dfs(0, 0, 1, 0)
+        
+        cnt = 0
+        
+        for x in grid:
+            for y in x:
+                cnt += (y == oo)
+        
+        return cnt == (n * n - 1)
+        
