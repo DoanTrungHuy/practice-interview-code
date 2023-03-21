20fa14961e5b1b95879d9287e0105f2a0d70ee01
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-03-21 23:45:52 +0700

    Time: 457 ms (55.04%), Space: 151.8 MB (14.22%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -4,8 +4,8 @@ public:
         int cnt = 0;
         
         for (char c = 'a'; c <= 'z'; ++c) {
-            unsigned long idx1 = s.find(c);
-            unsigned long idx2 = s.rfind(c);
+            unsigned int idx1 = s.find(c);
+            unsigned int idx2 = s.rfind(c);
             
             if (idx1 == string::npos or (idx2 - idx1 + 1) < 3) {
                 continue;
