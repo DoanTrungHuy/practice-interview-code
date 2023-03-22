694d5a06739d6e5ae5bdcbbe2932404980c3687e
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-03-22 18:24:53 +0700

    Time: 11 ms (38.50%), Space: 14 MB (9.68%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {        
+        int n = nums.size();
+        int dp[n][2];
+        int ans = nums[0];
+        
+        for (int i = 0; i < n; ++i) {
+            for (int state = 0; state <= 1; ++state) {
+                dp[i][state] = nums[i];
+            }
+        }
+        
+        for (int i = 1; i < n; ++i) {
+            for (int state = 0; state <= 1; ++state) {
+                ans = max({ans, nums[i], nums[i] * dp[i - 1][state]});
+            }
+            dp[i][0] = max({nums[i], nums[i] * dp[i - 1][1], nums[i] * dp[i - 1][0]});
+            dp[i][1] = min({nums[i], nums[i] * dp[i - 1][1], nums[i] * dp[i - 1][0]});
+        }
+        
+        return ans;
+    }
+};
