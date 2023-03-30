75e16fdff3ae1e67e8b828fd0f69f377a65443de
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-03-31 00:42:20 +0700

    Time: 263 ms (15.59%), Space: 14.8 MB (38.43%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.py
+++ 0087-scramble-string/0087-scramble-string.py
@@ -1,5 +1,7 @@
 class Solution:
-    @cache
+    def __init__(self):
+        self.dp = {}
+        
     def isScramble(self, s1: str, s2: str) -> bool:
         n = len(s1)
         
@@ -8,10 +10,15 @@ class Solution:
         if n == 1:
             return False
         
+        if (s1, s2) in self.dp:
+            return self.dp[(s1, s2)]
+        
         for i in range(1, n):
-            if self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:]):
-                return True
-            if self.isScramble(s1[:i], s2[n - i:]) and self.isScramble(s1[i:], s2[:n - i]):
+            if self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:]) or \
+            self.isScramble(s1[:i], s2[n - i:]) and self.isScramble(s1[i:], s2[:n - i]):
+                self.dp[(s1, s2)] = True
                 return True
             
+        self.dp[(s1, s2)] = False
+        
         return False
