2d2fb2c4d5890e80d966c0f6467cd2d70d760f17
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-04-02 23:00:06 +0700

    Time: 293 ms (100.00%), Space: 67 MB (75.00%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.ts
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.ts
@@ -1,21 +1,19 @@
 function lowerBound(potions: number[], success: number): number {
     const n = potions.length;
-    let left = 0, right = n - 1;
-    let ans = n;
+    let left = 0, right = n;
     
-    while (left <= right) {
+    while (left < right) {
         let mid = Math.floor((left + right) / 2);
         
         if (potions[mid] < success) {
             left = mid + 1;
         }
         else {
-            ans = mid;
-            right = mid - 1;
+            right = mid;
         }
     }
     
-    return ans;
+    return right;
 }
 
 function successfulPairs(spells: number[], potions: number[], success: number): number[] {
