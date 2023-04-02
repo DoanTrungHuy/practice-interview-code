48b13c4ea7df2b36e9ba7980699f3f6d48f9c707
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-04-02 23:03:15 +0700

    Time: 272 ms (30.02%), Space: 100 MB (12.80%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -8,7 +8,7 @@ public:
         {
             long long k = ceil(double(success*1.0 / spells[i]));
             
-            auto id = lower_bound(potions.begin(), potions.end(),k);
+            auto id = lower_bound(potions.begin(), potions.end(), k);
             ans.push_back(potions.size() - (id - potions.begin()));
         }
             
