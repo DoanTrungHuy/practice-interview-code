eb3cbf79624bf37fc790868aae5cb68db9a36c50
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-04-11 04:16:13 +0700

    Time: 3689 ms (7.09%), Space: 203.5 MB (8.55%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.py
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.py
@@ -1,18 +1,43 @@
 class Solution:
     def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
-        N = len(colors)                                     
-        incoming, Graph = [0]*N, [[] for _ in range(N)]     
-        for _, v in edges: incoming[v]+=1                   
-        for u, v in edges: Graph[u].append(v)               
-        stack = [u for u in range(N) if incoming[u]==0]     
-        cnts = [[0]*26 for _ in range(N)]                    
-
-        while stack:                                        
-            u = stack.pop()                                 
-            cnts[u][ord(colors[u])-ord('a')] += 1           
-            for v in Graph[u]:                              
-                cnts[v] = list(map(max, cnts[v], cnts[u]))  
-                incoming[v] -= 1                            
-                if incoming[v]==0: stack.append(v)          
-
-        return -1 if sum(incoming)>0 else max([max(node) for node in cnts])
+        adj = defaultdict(list)
+        
+        for u, v in edges:
+            adj[u].append(v)
+            
+        visited, path = set(), set()
+        n = len(colors)
+        count = [26 * [0] for _ in range(n)]
+        
+        def dfs(u):
+            if u in path:
+                return float('inf')
+            
+            if u in visited:
+                return 0
+            
+            path.add(u)
+            visited.add(u)
+            indexColor = ord(colors[u]) - ord('a')
+            count[u][indexColor] = 1
+            
+            for v in adj[u]:
+                if dfs(v) == float('inf'):
+                    return float('inf')
+                
+                for i in range(26):
+                    count[u][i] = max(
+                        count[u][i],
+                        int(indexColor == i) + count[v][i]
+                    )
+            
+            path.remove(u)
+            
+            return max(count[u])
+            
+        res = 0
+        
+        for u in range(n):
+            res = max(res, dfs(u))
+            
+        return -1 if res == float('inf') else res
