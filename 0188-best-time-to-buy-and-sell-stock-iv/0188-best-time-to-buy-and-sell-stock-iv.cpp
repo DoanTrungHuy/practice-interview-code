ad10b87917d5a9825f2db956344137731de0d8de
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-04-10 19:09:47 +0700

    Time: 10 ms (88.77%), Space: 11.8 MB (72.58%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,34 +1,27 @@
-int dp[1001][101][2];
-
 class Solution {
-private:
-    vector<int> prices;
-    int n;
-    
 public:
-    int dfs(int i, int k, int buy) {
-        if (k == 0 or i == n) {
-            return 0;
-        }
-        if (dp[i][k][buy] != -1) {
-            return dp[i][k][buy];
-        }
-        if (buy == 1) {
-            return dp[i][k][buy] = max(
-                -prices[i] + dfs(i + 1, k, 0), 
-                dfs(i + 1, k, 1)
-            );
-        }
-        return dp[i][k][buy] = max(
-            prices[i] + dfs(i + 1, k - 1, 1),
-            dfs(i + 1, k, 0)
-        );
-    }
-    
     int maxProfit(int k, vector<int>& prices) {
-        memset(dp, -1, sizeof(dp));
-        this->prices = prices;
-        this->n = prices.size();
-        return dfs(0, k, 1);
+        int dp[1001][101][2];
+        memset(dp, 0, sizeof(dp));
+        const int n = prices.size();
+        for (int i = n - 1; i >= 0; --i) {
+            for (int transaction = k; transaction >= 1; --transaction) {
+                for (int buy = 0; buy <= 1; ++buy) {
+                    if (buy == 1) {
+                        dp[i][transaction][buy] = max(
+                            -prices[i] + dp[i + 1][transaction][0],
+                            dp[i + 1][transaction][1]
+                        );
+                    }
+                    else {
+                        dp[i][transaction][buy] = max(
+                            prices[i] + dp[i + 1][transaction - 1][1],
+                            dp[i + 1][transaction][0]
+                        );
+                    }
+                }
+            }
+        }
+        return dp[0][k][1];
     }
 };
