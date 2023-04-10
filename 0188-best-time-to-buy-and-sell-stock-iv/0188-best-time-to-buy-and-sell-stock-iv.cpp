d6376ae1ec613532b5a4ad959377d30ca266dfa6
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-04-10 19:10:44 +0700

    Time: 16 ms (70.29%), Space: 11.6 MB (74.14%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -6,14 +6,20 @@ public:
         const int n = prices.size();
         for (int i = n - 1; i >= 0; --i) {
             for (int transaction = k; transaction >= 1; --transaction) {
-                dp[i][transaction][1] = max(
-                    -prices[i] + dp[i + 1][transaction][0],
-                    dp[i + 1][transaction][1]
-                );
-                dp[i][transaction][0] = max(
-                    prices[i] + dp[i + 1][transaction - 1][1],
-                    dp[i + 1][transaction][0]
-                );
+                for (int buy = 0; buy <= 1; ++buy) {
+                    if (buy == 1) {
+                        dp[i][transaction][buy] = max(
+                            -prices[i] + dp[i + 1][transaction][0],
+                            dp[i + 1][transaction][1]
+                        );
+                    }
+                    else {
+                        dp[i][transaction][buy] = max(
+                            prices[i] + dp[i + 1][transaction - 1][1],
+                            dp[i + 1][transaction][0]
+                        );
+                    }
+                }
             }
         }
         return dp[0][k][1];
