51088f0f7e36a874760d36c8f8d47a1c36357ab0
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-04-14 06:31:09 +0700

    Time: 2471 ms (85.40%), Space: 14.1 MB (31.14%) - LeetHub

---
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.py
@@ -0,0 +1,30 @@
+class Solution:
+    def largestVariance(self, s: str) -> int:
+        cnt = Counter(s)
+        res = 0
+        
+        for a, b in permutations(cnt, 2):
+            maxSub = 0
+            remain_a, remain_b = cnt[a], cnt[b]
+            has_a, has_b = False, False
+            
+            for c in s:
+                if c != a and c != b:
+                    continue
+                if maxSub < 0 and remain_a != 0 and remain_b != 0:
+                    maxSub = 0
+                    has_a, has_b = False, False
+                if c == a:
+                    maxSub += 1
+                    remain_a -= 1
+                    has_a = True
+                if c == b:
+                    maxSub -= 1
+                    remain_b -= 1
+                    has_b = True
+                if has_a and has_b:
+                    res = max(res, maxSub)
+                if remain_a == 0 and remain_b == 0:
+                    break
+            
+        return res
