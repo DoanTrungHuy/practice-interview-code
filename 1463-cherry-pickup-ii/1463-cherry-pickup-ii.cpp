b25eb6ac59fcc2af56c80258e3df33f6cfdc854a
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-04-14 05:01:28 +0700

    Time: 71 ms (92.68%), Space: 10 MB (91.59%) - LeetHub

---
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int cherryPickup(vector<vector<int>>& grid) {
+        const int n = grid.size(), m = grid[0].size();
+        
+        int dp[n][m][m];
+        memset(dp, -1, sizeof(dp));
+        int DIR[3] = {-1, 0, 1};
+        
+        for (int r = n - 1; r >= 0; --r) {
+            for (int c1 = 0; c1 < m; ++c1) {
+                for (int c2 = m - 1; c2 >= 0; --c2) {
+                    int collectCherry = c1 == c2 
+                        ? grid[r][c1] 
+                        : grid[r][c1] + grid[r][c2];
+                    
+                    if (r == n - 1) {
+                        dp[r][c1][c2] = collectCherry;
+                        continue;
+                    }
+                    
+                    int maxBottom = 0;
+                    
+                    for (int d1 : DIR) {
+                        for (int d2 : DIR) {
+                            int nc1 = c1 + d1;
+                            int nc2 = c2 + d2;
+                            
+                            if (0 <= nc1 and nc1 < m and 0 <= nc2 and nc2 < m) {
+                                maxBottom = max(maxBottom, dp[r + 1][nc1][nc2]);
+                            }
+                        }
+                    }
+                    
+                    dp[r][c1][c2] = maxBottom + collectCherry;
+                }
+            }
+        }
+        
+        return dp[0][0][m - 1];
+    }
+};
