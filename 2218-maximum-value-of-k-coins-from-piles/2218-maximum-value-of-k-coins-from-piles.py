a38c4d15fd0e51b95ef9461e6ddf396cc03db671
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-04-16 14:59:09 +0700

    Time: 4441 ms (80.75%), Space: 141.1 MB (14.29%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.py
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.py
@@ -3,16 +3,16 @@ class Solution:
         n = len(piles)
         
         @lru_cache(None)
-        def dp(i, coins):
-            if i == n or coins == 0:
+        def dp(i, k):
+            if i == n or k == 0:
                 return 0
             
-            res = dp(i + 1, coins)        
+            res = dp(i + 1, k)        
             currPiles = 0
     
-            for j in range(min(coins, len(piles[i]))):
+            for j in range(min(k, len(piles[i]))):
                 currPiles += piles[i][j]
-                res = max(res, currPiles + dp(i + 1, coins - j - 1))
+                res = max(res, currPiles + dp(i + 1, k - j - 1))
                 
             return res
         
