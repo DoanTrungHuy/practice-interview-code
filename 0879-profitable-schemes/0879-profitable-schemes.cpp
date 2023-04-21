944a5dd72ce1338623a5fede0498be6bb28feea2
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-04-21 15:11:19 +0700

    Time: 5977 ms (5.71%), Space: 669.5 MB (5.71%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.cpp
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -1,35 +1,20 @@
-const int MAXN = 102, MOD = 1e9 + 7, MAXPROFIT = 101;
-int memo[MAXN][MAXN][MAXN];
-
-class Solution {
-private:
-    vector<int> group, profit;
-    int minProfit;
-    
-public:
-    int dp(int i, int p, int n) {
-        if (i == group.size()) {
-            return p >= minProfit;
-        }
+class Solution:
+    def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
+        MOD = 10**9 + 7
+        memo = {}
         
-        if (memo[i][p][n] != -1) {
-            return memo[i][p][n];
-        }
+        def dp(i, p, n):
+            if i == len(group):
+                return 1 if p >= minProfit else 0
+            
+            if (i, p, n) in memo:
+                return memo[(i, p, n)]
+            
+            res = dp(i + 1, p, n) % MOD
+            if n - group[i] >= 0:
+                res = (res + dp(i + 1, min(p + profit[i], 101), n - group[i])) % MOD
+                
+            memo[(i, p, n)] = res
+            return res
         
-        memo[i][p][n] = dp(i + 1, p, n) % MOD;
-        
-        if (n - group[i] >= 0) {
-            memo[i][p][n] = (memo[i][p][n] + dp(i + 1, min(p + profit[i], MAXPROFIT), n - group[i])) % MOD;
-        }
-        
-        return memo[i][p][n];
-    }
-    
-    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
-        memset(memo, -1, sizeof(memo));
-        this->group = group;
-        this->profit = profit;
-        this->minProfit = minProfit;
-        return dp(0, 0, n);
-    }
-};
+        return dp(0, 0, n)
