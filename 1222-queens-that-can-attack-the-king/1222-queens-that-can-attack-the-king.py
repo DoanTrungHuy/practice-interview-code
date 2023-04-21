fdf809471f6c0cfff74be688a70fc12ea649b150
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-04-21 13:17:19 +0700

    Time: 33 ms (94.59%), Space: 13.8 MB (84.46%) - LeetHub

---
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
@@ -0,0 +1,18 @@
+class Solution:
+    def queensAttacktheKing(self, queens: List[List[int]], king: List[int]) -> List[List[int]]:
+        checkQueens = set((x, y) for x, y in queens)
+        x = king[0]
+        y = king[1]
+        res = []
+        
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                for spill in range(0, 8):
+                    nx = x + dx*spill
+                    ny = y + dy*spill
+                    
+                    if (nx, ny) in checkQueens:
+                        res.append([nx, ny])
+                        break
+                        
+        return res
