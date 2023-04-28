29ad4fe2e94feaef956b79c746ba26b30fc2b9ca
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-04-29 02:11:41 +0700

    Time: 40 ms (97.31%), Space: 9.6 MB (97.07%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.cpp
+++ 1416-restore-the-array/1416-restore-the-array.cpp
@@ -1,25 +1,26 @@
-class Solution:
-    def numberOfArrays(self, s: str, k: int) -> int:
-        MOD = 10**9 + 7
-        n = len(s)
+class Solution {
+public:
+    int numberOfArrays(string s, int k) {
+        const int MOD = 1e9 + 7;
+        int n = s.size();
+        int dp[n + 1];
+        memset(dp, 0, sizeof(dp));
+        dp[n] = 1;
         
-        @lru_cache(None)
-        def dp(i):
-            if i == n:
-                return 1
-            
-            if s[i] == '0':
-                return 0
-            
-            ans = 0
-            num = 0
-            
-            for j in range(i, n):
-                num = num*10 + (ord(s[j]) - ord('0'))
-                if num > k:
-                    break
-                ans = (ans + dp(j + 1)) % MOD
-                
-            return ans
+        for (int i = n - 1; i >= 0; --i) {
+            long long num = 0;
+            if (s[i] == '0') {
+                continue;
+            }
+            for (int j = i; j < n; ++j) {
+                num = num*10 + (s[j] - '0');
+                if (num > k) {
+                    break;
+                }
+                dp[i] = (dp[i] + dp[j + 1]) % MOD;
+            }
+        }
         
-        return dp(0)
+        return dp[0];
+    }
+};
