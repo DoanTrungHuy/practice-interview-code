ac102a6743eca640d820aa81a6bec0e8e0ccabe4
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-04-29 13:38:14 +0700

    Time: 10851 ms (21.73%), Space: 40.7 MB (5.01%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -2,14 +2,17 @@ class Solution:
     def maxCoins(self, nums: List[int]) -> int:
         nums = [1] + nums + [1]
         n = len(nums)
+        memo = {}
         
-        @lru_cache(None)
         def dp(i, j):
             if i >= j:
                 return 0
+            if (i, j) in memo:
+                return memo[(i, j)]
             ans = 0
             for k in range(i + 1, j):
                 ans = max(ans, nums[i] * nums[k] * nums[j] + dp(i, k) + dp(k, j))
+            memo[(i, j)] = ans
             return ans
         
         return dp(0, n - 1)
