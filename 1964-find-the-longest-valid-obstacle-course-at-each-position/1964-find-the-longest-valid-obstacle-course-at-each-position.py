b02f716d53d80875cdf7cb8d9bdebac65eec2863
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-05-07 18:41:08 +0700

    Time: 1529 ms (81.82%), Space: 35.1 MB (7.57%) - LeetHub

---
+++ 1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.py
@@ -0,0 +1,10 @@
+class Solution:
+    def longestObstacleCourseAtEachPosition(self, obstacles: List[int]) -> List[int]:
+        dp, lis = [10**8] * (len(obstacles) + 1), []
+        
+        for obstacle in obstacles:
+            index = bisect_right(dp, obstacle)
+            lis.append(index + 1)
+            dp[index] = obstacle
+        
+        return lis
