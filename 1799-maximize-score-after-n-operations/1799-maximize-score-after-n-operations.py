f3ab1bab4a714c2fbfa872f2ad9c957f7527d571
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-05-14 12:55:12 +0700

    Time: 2232 ms (58.23%), Space: 22.7 MB (66.46%) - LeetHub

---
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxScore(self, nums: List[int]) -> int:
+        m = len(nums)
+        n = m // 2
+        memo = defaultdict(int)
+        
+        def dp(op, mask):
+            if op > n:
+                return 0
+            
+            if mask in memo:
+                return memo[mask]
+            
+            for i in range(m):
+                if mask & (1 << i):
+                    continue
+                for j in range(i + 1, m):
+                    if mask & (1 << j):
+                        continue
+                    newMask = mask | (1 << i) | (1 << j)
+                    score = op * gcd(nums[i], nums[j]) + dp(op + 1, newMask)
+                    memo[mask] = max(memo[mask], score)
+                    
+            return memo[mask]
+            
+        return dp(1, 0)
