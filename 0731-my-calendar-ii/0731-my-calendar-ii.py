91b426323f6fd1b2e1bf3c531413488837272455
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-05-22 11:32:44 +0700

    Time: 899 ms (32.19%), Space: 17.5 MB (8.43%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.py
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -3,24 +3,24 @@ from sortedcontainers import SortedList
 class MyCalendarTwo:
 
     def __init__(self):
-        self.sl = SortedList()
+        self.line = SortedList()
 
     def book(self, start: int, end: int) -> bool:
-        self.sl.add((start, 1))
-        self.sl.add((end, -1))
+        self.line.add((start, 1))
+        self.line.add((end, -1))
         
         group = 0
         ans = True
         
-        for element in self.sl:
+        for element in self.line:
             group += element[1]
             if group == 3:
                 ans = False
                 break
         
         if not ans:
-            self.sl.discard((start, 1))
-            self.sl.discard((end, -1))
+            self.line.discard((start, 1))
+            self.line.discard((end, -1))
             
         return ans
 
