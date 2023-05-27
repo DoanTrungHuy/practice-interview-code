5b5800d08011a9ed857441b1d03ea99f75afe87c
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-05-27 23:29:23 +0700

    Time: 80 ms (22.22%), Space: 16.2 MB (11.11%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -2,6 +2,6 @@ class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
         prices.sort()
         s = prices[0] + prices[1]
-        if s<= money:
+        if s <= money:
             money -= s
         return money
