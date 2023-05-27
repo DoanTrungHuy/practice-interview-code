98d97d2112d3e97a036db9038927bc1fa05a7665
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-05-27 11:30:17 +0700

    Time: 0 ms (100.00%), Space: 7.6 MB (44.61%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,27 +1,31 @@
-int memo[501];
-
 class Solution {
 private:
-    vector<int> piles;
-    int n;
-    
+	vector<int> piles;
+	int n;
+
 public:
-    int dp(int i) {
-        if (i == n / 2) {
-            return 0;
-        }
-        if (memo[i] != -1) {
-            return memo[i];
-        }
-        int left = piles[i] + dp(i + 1);
-        int right = piles[n - i - 1] + dp(i + 1);
-        return memo[i] = max(left, right);
-    }
+	int memo[501];
+
+	int dp(int i) {
+		if (i == n / 2) {
+			return 0;
+		}
+		if (memo[i] != -1) {
+			return memo[i];
+		}
+		int head = piles[i] + dp(i + 1);
+		int tail = piles[n - 1 - i] + dp(i + 1);
+		return memo[i] = max(head, tail);
+	}
     bool stoneGame(vector<int>& piles) {
-        memset(memo, -1, sizeof(memo));
-        this->piles = piles;
-        this->n = piles.size();
-        int alice = dp(0);
-        return alice > accumulate(piles.begin(), piles.end(), 0) - alice;
+		this->piles = piles;
+		this->n = piles.size();
+		memset(memo, -1, sizeof(memo));
+		int alice = dp(0);
+		int total = 0;
+		for (int i = 0; i < n; ++i) {
+			total += piles[i];
+		}
+		return 2 * alice > total;
     }
 };
