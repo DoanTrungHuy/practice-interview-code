50fbc3cfe60fafe834f7e605f87f324818ffac1b
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-06-10 04:02:35 +0700

    Time: 7604 ms (5.12%), Space: 603.3 MB (5.12%) - LeetHub

---
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.py
@@ -0,0 +1,31 @@
+from sortedcontainers import SortedList, SortedSet, SortedDict
+
+class Solution:
+    def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
+        line = defaultdict(SortedSet)
+        
+        for start, end in intervals:
+            line[start].add((1, end - start + 1))
+            line[end + 1].add((2, end - start + 1))
+            
+        for i, time in enumerate(queries):
+            line[time].add((3, i))
+            
+        line = SortedDict(line)
+        
+        sizes = SortedList()
+        m = len(queries)
+        ans = [-1 for _ in range(m)]
+        
+        for time in line:
+            for choose, size_or_index in line[time]:
+                if choose == 1:
+                    sizes.add(size_or_index)
+                elif choose == 2:
+                    if size_or_index in sizes:
+                        sizes.remove(size_or_index)
+                elif choose == 3:
+                    if sizes:
+                        ans[size_or_index] = sizes[0]
+                        
+        return ans
