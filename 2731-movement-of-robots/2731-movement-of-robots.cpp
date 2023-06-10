cda68f76bd34f947c26831a05a410c4eab08a704
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-06-11 02:37:29 +0700

    Time: 165 ms (66.67%), Space: 93.2 MB (66.67%) - LeetHub

--- 2731-movement-of-robots/2731-movement-of-robots.cpp
+++ 2731-movement-of-robots/2731-movement-of-robots.cpp
@@ -15,15 +15,14 @@ public:
         }
         
         sort(nums.begin(), nums.end());
-        
-        ll total = 0, ans = 0;
+        ll ans = 0, pref = 0;
         const ll MOD = 1e9 + 7;
         
         for (int i = 0; i < n; ++i) {
-            ans = (ans + (1LL*nums[i]*i)%MOD - total)%MOD;
-            total = (total + nums[i]) % MOD;
+            ans = (ans + 1LL*nums[i]*i - pref) % MOD;
+            pref = (pref + nums[i]) % MOD;
         }
         
-        return (int)(ans + MOD)%MOD;
+        return ans;
     }
 };
