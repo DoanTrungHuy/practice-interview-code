2680e005401258c199f37d22eb6705cc8f015b07
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-06-15 21:14:40 +0700

    Time: 31 ms (17.24%), Space: 15.5 MB (9.20%) - LeetHub

--- 0850-rectangle-area-ii/0850-rectangle-area-ii.cpp
+++ 0850-rectangle-area-ii/0850-rectangle-area-ii.cpp
@@ -6,27 +6,25 @@ const ll MOD = 1e9 + 7;
 
 class Solution {
 public:
-    ll calc(multiset<pair<ll, ll>> &line, ll width) {
+    ll calc(multiset<pair<ll, ll>> line, ll dist_x) {
         ll prev = INT_MIN;
-        ll total = 0LL;
-        
-        for (auto [bottom, up] : line) {
-            prev = max(prev, bottom);
-            total = (total + max(0LL, (up - prev)*width)%MOD)%MOD;
-            prev = max(prev, up);
+        ll ans = 0;
+        for (auto [l, r] : line) {
+            prev = max(prev, l);
+            ans = (ans + max(0LL, (r - prev)*dist_x)) % MOD;
+            prev = max(prev, r);
         }
-        
-        return total;
+        return ans;
     }
     
     int rectangleArea(vector<vector<int>>& rectangles) {
         vector<vector<ll>> events;
         
         for (vector<int> rectangle : rectangles) {
-            ll x1 = rectangle[0]*1LL;
-            ll y1 = rectangle[1]*1LL;
-            ll x2 = rectangle[2]*1LL;
-            ll y2 = rectangle[3]*1LL;
+            ll x1 = rectangle[0];
+            ll y1 = rectangle[1];
+            ll x2 = rectangle[2];
+            ll y2 = rectangle[3];
             
             events.push_back({x1, INSERT, y1, y2});
             events.push_back({x2, DELETE, y1, y2});
@@ -35,18 +33,20 @@ public:
         sort(events.begin(), events.end(), [&](vector<ll> a, vector<ll> b) {
             if (a[0] == b[0]) {
                 return a[1] < b[1];
-            }
+            } 
             return a[0] < b[0];
         });
         
         multiset<pair<ll, ll>> line;
         ll prev = INT_MIN;
-        ll ans = 0LL;
+        ll ans = 0;
+        
         
         for (vector<ll> event : events) {
             ll curr = event[0];
-            ll choose = event[1];
-            ll y1 = event[2], y2 = event[3];
+            int choose = event[1];
+            ll y1 = event[2];
+            ll y2 = event[3];
             
             if (prev != INT_MIN) {
                 ans = (ans + calc(line, curr - prev)) % MOD;
@@ -62,6 +62,6 @@ public:
             prev = curr;
         }
         
-        return (int)ans;
+        return ans;
     }
 };
