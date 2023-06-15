4aaefddb29113f1cbbc5043ba964c1f47eb44211
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-06-15 20:52:54 +0700

    Time: 940 ms (8.69%), Space: 196.3 MB (8.69%) - LeetHub

--- 2382-maximum-segment-sum-after-removals/2382-maximum-segment-sum-after-removals.cpp
+++ 2382-maximum-segment-sum-after-removals/2382-maximum-segment-sum-after-removals.cpp
@@ -1,70 +1,43 @@
 using ll = long long;
 
-class DSU {
-private:
-    vector<ll> parent, sum;
-    ll maxSegmentSum;
-    
-public:
-    DSU(vector<int> nums) {
-        int n = nums.size();
-        parent.resize(n), sum.resize(n);
-        maxSegmentSum = 0;
-        
-        for (int i = 0; i < n; ++i) {
-            parent[i] = i;
-            sum[i] = 1LL*nums[i];
-        } 
-    }
-    
-    ll find(int u) {
-        if (u == parent[u]) {
-            return u;
-        }
-        return parent[u] = find(parent[u]);
-    }
-    
-    void merge(int u, int v) {
-        int pu = find(u);
-        int pv = find(v);
-        
-        if (pu != pv) {
-            parent[pv] = pu;
-            sum[pu] += sum[pv];
-            maxSegmentSum = max(maxSegmentSum, sum[pu]);
-        }
-    }
-    
-    ll getMaxSegmentSum() {
-        return maxSegmentSum;
-    }
-    
-    void setMaxSegmentSum(ll val) {
-        maxSegmentSum = max(maxSegmentSum, val);
-    }
-};
-
 class Solution {
 public:
     vector<long long> maximumSegmentSum(vector<int>& nums, vector<int>& removeQueries) {
-        DSU dsu(nums);
-        int n = removeQueries.size();
-        vector<ll> ans(n);
-        vector<bool> visited(n, false);
+        int n = nums.size();
+        vector<ll> prefix(n + 1, 0);
         
-        for (int i = n - 1; i >= 0; --i) {
-            int positive = removeQueries[i];
+        for (int i = 0; i < n; ++i) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+        
+        vector<ll> ans;
+        
+        set<int> removeIdx;
+        removeIdx.insert(0);
+        removeIdx.insert(n + 1);
+        
+        multiset<ll> maxSegmentSum;
+        maxSegmentSum.insert(prefix[n]);
+        
+        for (int idx : removeQueries) {
+            idx++;
+            auto upper = removeIdx.lower_bound(idx);
+            auto lower = prev(upper);
             
-            ans[i] = dsu.getMaxSegmentSum();
-            visited[positive] = true;
-            dsu.setMaxSegmentSum(nums[positive]);
+            int hi = *upper;
+            int lo = *lower;
             
-            if (positive - 1 >= 0 and visited[positive - 1]) {
-                dsu.merge(positive, positive - 1);
-            }
-            if (positive + 1 < n and visited[positive + 1]) {
-                dsu.merge(positive, positive + 1);
-            }
+            ll sumOld = prefix[hi - 1] - prefix[lo];
+            ll sum1 = prefix[idx - 1] - prefix[lo];
+            ll sum2 = prefix[hi - 1] - prefix[idx];
+            
+            maxSegmentSum.erase(maxSegmentSum.lower_bound(sumOld));
+            maxSegmentSum.insert(sum1);
+            maxSegmentSum.insert(sum2);
+            
+            ans.push_back(*maxSegmentSum.rbegin());
+            
+            removeIdx.insert(idx);
         }
         
         return ans;
