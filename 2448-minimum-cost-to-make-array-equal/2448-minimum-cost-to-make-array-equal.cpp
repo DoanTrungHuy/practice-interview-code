a4463f29e4172e0dbc00e28a72599f9fac14eb6b
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-06-24 18:39:44 +0700

    Time: 130 ms (24.19%), Space: 38.5 MB (73.08%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -1,38 +1,33 @@
 using ll = long long;
 
 class Solution {
-private:
-    vector<pair<int, int>> bag;
-    int n;
-    vector<int> cost;
-    
 public:
-    ll minCost(vector<int>& nums, vector<int>& cost) {
-        this->cost = cost;
-        int n = cost.size();
+    ll f(vector<int> &nums, vector<int> &cost, int medium) {
+        int n = nums.size();
         ll total = 0;
         
         for (int i = 0; i < n; ++i) {
-            bag.push_back({nums[i], cost[i]});
-            total += 1LL*cost[i];
+            total += 1LL*abs(nums[i] - medium)*cost[i];
         }
         
-        sort(bag.begin(), bag.end());
+        return total;
+    }
+    
+    ll minCost(vector<int>& nums, vector<int>& cost) {
+        int left = 1, right = 1e6;
+        ll ans = 0;
         
-        ll sumCurrent = 0;
-        int medium = 0;
-        int i = 0;
-        
-        while (sumCurrent <= total / 2 and i < n) {
-            sumCurrent += 1L*bag[i].second;
-            medium = bag[i].first;
-            i++;
-        }
-        
-        ll ans = 0LL;
-        
-        for (int i = 0; i < n; ++i) {
-            ans += 1LL*abs(bag[i].first - medium)*bag[i].second;
+        while (left < right) {
+            int x = (left + right) / 2;
+            ll y1 = f(nums, cost, x);
+            ll y2 = f(nums, cost, x + 1);
+            if (y1 < y2) {
+                right = x;    
+            }
+            else {
+                ans = y2;
+                left = x + 1;
+            }
         }
         
         return ans;
