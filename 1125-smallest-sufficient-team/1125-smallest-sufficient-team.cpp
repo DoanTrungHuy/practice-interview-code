939bb99cf4c321ff2e07ffb614e10442a6b70cd1
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-07-16 18:06:31 +0700

    Time: 346 ms (35.80%), Space: 43.5 MB (43.55%) - LeetHub

---
+++ 1125-smallest-sufficient-team/1125-smallest-sufficient-team.cpp
@@ -0,0 +1,68 @@
+class Solution {
+private:
+    unordered_map<string, int> compress;
+    int m, n;
+    vector<long long> pMask;
+    
+public:
+    long long memo[61][1 << 16];
+    
+    long long dp(int i, int mask) {
+        if (mask == 0) {
+            return 0LL;
+        }
+        
+        if (i == n) {
+            return (1LL << n) - 1;
+        }
+        
+        if (memo[i][mask] != -1) {
+            return memo[i][mask];
+        }
+        
+        long long not_take = dp(i + 1, mask);
+        long long take = (1LL << n) - 1;
+        
+        if ((mask & ~pMask[i]) != mask) {
+            take = (1LL << i) | dp(i + 1, mask & ~pMask[i]);
+        }
+        
+        if (__builtin_popcountll(take) <= __builtin_popcountll(not_take)) {
+            return memo[i][mask] = take;
+        }
+        
+        return memo[i][mask] = not_take;
+    }
+    
+    vector<int> smallestSufficientTeam(vector<string>& req_skills, vector<vector<string>>& people) {
+        m = req_skills.size();
+        n = people.size();
+        memset(memo, -1, sizeof(memo));
+        
+        for (int i = 0; i < m; ++i) {
+            compress[req_skills[i]] = i;
+        }
+        
+        for (int i = 0; i < n; ++i) {
+            long long mask = 0;
+            for (int j = 0; j < people[i].size(); ++j) {
+                mask |= (1LL << compress[people[i][j]]);
+            } 
+            pMask.push_back(mask);
+        }
+        
+        long long mask = dp(0, (1 << m) - 1);
+        
+        vector<int> ans;
+        
+        cout << mask << ' ';
+
+        for (int i = 0; i < n; ++i) {
+            if (mask & (1LL << i)) {
+                ans.push_back(i);
+            }
+        }
+        
+        return ans;
+    }
+};
