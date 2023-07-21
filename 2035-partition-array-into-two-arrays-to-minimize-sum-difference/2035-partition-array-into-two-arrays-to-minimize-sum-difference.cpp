33011a647c721280a4dc8aaff934093828ea7f43
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-07-21 19:49:45 +0700

    Time: 1075 ms (18.89%), Space: 40.4 MB (98.12%) - LeetHub

--- 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
+++ 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -5,7 +5,7 @@ public:
         int m = n / 2;
         vector<vector<int>> arr1_diff_sub(m + 1);
         
-        for (int mask = 0; mask <= (1 << m); ++mask) {
+        for (int mask = 0; mask < (1 << m); ++mask) {
             int sum = 0;
             int len = 0;
             for (int i = 0; i < m; ++i) {
@@ -26,7 +26,7 @@ public:
         
         int minDiff = INT_MAX;
         
-        for (int mask = 0; mask <= (1 << (n - m)); ++mask) {
+        for (int mask = 0; mask < (1 << (n - m)); ++mask) {
             int sum = 0;
             int len = 0;
             
@@ -40,17 +40,16 @@ public:
                 }
             }
             
-            int num2 = sum;
+            int diff2 = sum;
             
-            auto it = lower_bound(arr1_diff_sub[len].begin(), arr1_diff_sub[len].end(), num2);
+            auto it = lower_bound(arr1_diff_sub[m - len].begin(), arr1_diff_sub[m - len].end(), -diff2);
             
-            if (it != arr1_diff_sub[len].end()) {
-                minDiff = min(minDiff, abs(num2 - *it));
+            if (it != arr1_diff_sub[m - len].end()) {
+                minDiff = min(minDiff, abs(diff2 + *it));
             }
             
-            if (it != arr1_diff_sub[len].begin()) {
-                it--;
-                minDiff = min(minDiff, abs(num2 - *it));
+            if (it != arr1_diff_sub[m - len].begin()) {
+                minDiff = min(minDiff, abs(diff2 + *prev(it)));
             }
         }
         
