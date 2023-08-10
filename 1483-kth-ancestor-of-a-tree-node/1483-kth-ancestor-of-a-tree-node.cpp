28adcf0529e9100f0032087875338aba544accff
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-08-10 16:18:58 +0700

    Time: 285 ms (88.12%), Space: 115.7 MB (70.20%) - LeetHub

--- 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.cpp
+++ 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.cpp
@@ -1,33 +1,32 @@
 class TreeAncestor {
 private:
-    vector<vector<int>> up;
+    vector<vector<int>> par;
     int m;
     
 public:
     TreeAncestor(int n, vector<int>& parent) {
         this->m = log2(n) + 1;
-        up.resize(n, vector<int>(m, -1));
+        par.resize(n, vector<int>(m, -1));
         
         for (int u = 0; u < n; ++u) {
-            up[u][0] = parent[u];
+            par[u][0] = parent[u];
         }
         
         for (int i = 1; i < m; ++i) {
             for (int u = 0; u < n; ++u) {
-                if (up[u][i - 1] == -1) {
-                    continue;
+                if (par[u][i - 1] != -1) {
+                    par[u][i] = par[par[u][i - 1]][i - 1];
                 }
-                up[u][i] = up[up[u][i - 1]][i - 1];
             }
         }
     }
     
     int getKthAncestor(int node, int k) {
-        for (int i = 0; i < m; ++i) {
+        for (int i = m; i >= 0; --i) {
             if ((k >> i) & 1) {
-                node = up[node][i];
+                node = par[node][i];
                 if (node == -1) {
-                    return -1;
+                    return node;
                 }
             }
         }
