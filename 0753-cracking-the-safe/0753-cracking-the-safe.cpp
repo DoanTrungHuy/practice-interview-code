6fdb313fe5a24cfcd9485978d448ea97c5909453
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-08-14 13:26:03 +0700

    Time: 13 ms (71.19%), Space: 10.2 MB (30.68%) - LeetHub

--- 0753-cracking-the-safe/0753-cracking-the-safe.cpp
+++ 0753-cracking-the-safe/0753-cracking-the-safe.cpp
@@ -3,27 +3,39 @@ private:
     string ans;
     int n, k;
     unordered_set<string> visited;
+    vector<string> s;
+    int m;
     
 public:
-    void dfs(string last) {
+    bool dfs(string last) {
+        if (visited.size() == m) {
+            return true;
+        }
         for (char c = 0; c < k; ++c) {
             string tmp = last + char(c + '0');
             if (!visited.count(tmp)) {
                 visited.insert(tmp);
-                dfs(tmp.substr(1));
                 ans.push_back(c + '0');
+                if (dfs(tmp.substr(1))) {
+                    return true;
+                }
+                ans.pop_back();
+                visited.erase(tmp);
             }
         }
+        return false;
     }
     
     string crackSafe(int n, int k) {
         this->n = n;
         this->k = k;
+        this->m = pow(k, n);
         string start = "";
         for (int i = 0; i < n - 1; ++i) {
             start.push_back('0');
         }
+        ans = start;
         dfs(start);
-        return ans + start;
+        return ans;
     }
 };
