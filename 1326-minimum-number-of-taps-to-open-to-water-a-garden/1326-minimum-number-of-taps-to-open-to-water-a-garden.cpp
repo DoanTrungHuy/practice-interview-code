0c97ada87c6bf47c97831f418834eed8e6b0240b
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-08-31 12:27:39 +0700

    Time: 47 ms (37.74%), Space: 17 MB (16.15%) - LeetHub

---
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -0,0 +1,64 @@
+const int MN = 1e4;
+int memo[MN];
+using ar = array<int, 2>;
+const int INF = 1e8;
+
+class Solution {
+private:
+    vector<ar> events;
+    int n, m;
+    
+public:
+    int dp(int i) {
+        if (events[i][1] == n) {
+            return 1;
+        }
+        if (i == m) {
+            return INF;
+        }
+        if (memo[i] != -1) {
+            return memo[i];
+        }
+        int ans = INF;
+        for (int j = i + 1; j < m; ++j) {
+            if (events[i][1] < events[j][0]) {
+                break;
+            }
+            ans = min(ans, 1 + dp(j));
+        }
+        return memo[i] = ans;
+    }
+    
+    int minTaps(int n, vector<int>& ranges) {
+        this->n = n;
+        memset(memo, -1, sizeof(memo));
+        this->m = ranges.size();
+        
+        for (int i = 0; i < m; ++i) {
+            events.push_back({
+                max(0, i - ranges[i]), 
+                min(n, i + ranges[i])
+            });
+        }
+        
+        sort(events.begin(), events.end(), [&](const ar a, const ar b){
+            if (a[0] == b[0]) {
+                return a[1] > b[1];
+            }
+            return a[0] < b[0];
+        });
+        
+        int ans = INF;
+        
+        for (int i = 0; i < m; ++i) {
+            int start = events[i][0];
+            int end = events[i][1];
+            
+            if (start == 0) {
+                ans = min(ans, dp(i));
+            }
+        }
+        
+        return ans == INF ? -1 : ans;
+    }
+};
