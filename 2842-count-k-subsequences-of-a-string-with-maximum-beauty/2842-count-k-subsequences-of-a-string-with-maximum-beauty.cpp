aa2910f8fa4bec0426a38220fd54be5fc761e951
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-09-03 00:40:04 +0700

    Time: 2035 ms (10.00%), Space: 20.9 MB (30.00%) - LeetHub

---
+++ 2842-count-k-subsequences-of-a-string-with-maximum-beauty/2842-count-k-subsequences-of-a-string-with-maximum-beauty.cpp
@@ -0,0 +1,68 @@
+using ll = long long;
+const int MN = 1e6 + 1;
+ll p[MN];
+const ll MOD = 1e9 + 7;
+
+class Solution {
+public:
+    ll exp(ll a, ll b) {
+        if (b == 0) {
+            return 1;
+        }
+        ll res = exp(a, b / 2);
+        res = (res * res) % MOD;
+        if (b & 1) {
+            return (res * a) % MOD;
+        }
+        return res;
+    }
+    
+    ll C(ll n, ll k) {
+        ll res = p[n];
+        res = (res * exp(p[n - k], MOD - 2)) % MOD;
+        res = (res * exp(p[k], MOD - 2)) % MOD;
+        return res;
+    }
+    
+    int countKSubsequencesWithMaxBeauty(string s, int k) {
+        unordered_map<char, ll> cnt_characters;
+        
+        for (char c : s) {
+            cnt_characters[c]++;
+        }
+        
+        if (cnt_characters.size() < k) {
+            return 0;
+        }
+        
+        map<int, ll> cnt_freqs;
+        
+        for (const auto [_, c] : cnt_characters) {
+            cnt_freqs[c]++;
+        }
+        
+        p[0] = 1;
+        for (int i = 1; i < MN; ++i) {
+            p[i] = (p[i - 1] * i) % MOD;
+        }
+        
+        ll ans = 1LL;
+        
+        for (auto it = cnt_freqs.rbegin(); it != cnt_freqs.rend(); ++it) {
+            ll v = it->first;
+            ll c = it->second;
+            
+            if (k >= c) {
+                ans = (ans * exp(v, c)) % MOD;
+                k -= c;
+            }
+            else {
+                ans = (ans * C(c, k)) % MOD;
+                ans = (ans * exp(v, k)) % MOD;
+                break;
+            }
+        }
+        
+        return ans;
+    }
+};
