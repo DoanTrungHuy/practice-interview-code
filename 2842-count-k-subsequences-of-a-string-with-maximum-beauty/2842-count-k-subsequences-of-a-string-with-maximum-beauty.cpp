613e54df8ddcf613a66e72141150f1f44b46ce81
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-09-03 01:39:29 +0700

    Time: 2433 ms (10.00%), Space: 20.4 MB (30.00%) - LeetHub

--- 2842-count-k-subsequences-of-a-string-with-maximum-beauty/2842-count-k-subsequences-of-a-string-with-maximum-beauty.cpp
+++ 2842-count-k-subsequences-of-a-string-with-maximum-beauty/2842-count-k-subsequences-of-a-string-with-maximum-beauty.cpp
@@ -1,54 +1,76 @@
+using ll = long long;
+const int MN = 1e6 + 1;
+ll p[MN];
+const ll MOD = 1e9 + 7;
+
 class Solution {
 public:
+    ll exp(ll a, ll b) {
+        if (b == 0) {
+            return 1;
+        }
+        ll res = exp(a, b / 2);
+        res = (res * res) % MOD;
+        if (b & 1) {
+            return (res * a) % MOD;
+        }
+        return res;
+    }
+    
+    ll C(ll n, ll k) {
+        ll res = p[n];
+        res = (res * exp(p[n - k], MOD - 2)) % MOD;
+        res = (res * exp(p[k], MOD - 2)) % MOD;
+        return res;
+    }
+    
     int countKSubsequencesWithMaxBeauty(string s, int k) {
-        const long long mod = 1000000007LL;
-        vector <vector <long long> > C; 
-        C.resize(27);
-        for (int i = 0; i <= 26; i++) {
-            C[i].resize(27, 0LL); 
+        vector<ll> cnt(26, 0);
+        
+        for (char c : s) {
+            cnt[c - 'a']++;
         }
-        for (int i = 0; i <= 26; i++) {
-            C[i][i] = 1;
-            C[i][0] = 1;
+        
+        if (cnt.size() < k) {
+            return 0;
         }
-        for (int i = 1; i <= 26; i++) {
-            for (int j = 1; j < i; j++) {
-                C[i][j] = (C[i - 1][j] + C[i - 1][j - 1]) % mod; 
-            }
+        
+        p[0] = 1LL;
+        
+        for (int i = 1; i < MN; ++i) {
+            p[i] = (p[i - 1] * i) % MOD;
         }
-        vector <int> cnt;
-        cnt.clear();
-        cnt.resize(26, 0); 
-        for (int i = 0; i < (int) s.size(); i++) {
-            cnt[s[i] - 'a']++; 
-        }
-        sort(cnt.begin(), cnt.end()); 
-        long long ans = 1LL; 
-        for (int i = 25; i >= 0; i--) {
+        
+        sort(cnt.begin(), cnt.end());
+        
+        long long ans = 1LL;
+        
+        for (int i = 25; i >= 0; --i) {
             if (k > 0) {
-                (ans *= (long long) cnt[i]) %= mod; 
-                k--; 
-                if (k == 0) {    
-                    int cntEqual = 0;
+                (ans *= cnt[i]) %= MOD;
+                k -= 1;
+                if (k == 0) {
+                    int total_equal = 0;
                     int p = -1; 
                     for (int j = i; j <= 25; j++) {
                         if (cnt[j] == cnt[i]) {
-                            cntEqual++; 
+                            total_equal++; 
                             p = j; 
                         }
                     }
                     for (int j = i - 1; j >= 0; j--) {
                         if (cnt[j] == cnt[i]) {
-                            cntEqual++; 
+                            total_equal++; 
                         }
                     }
-                    (ans *= C[cntEqual][p - i + 1]) %= mod;
+                    (ans *= C(total_equal, p - i + 1)) %= MOD;
                 }
             }
-            else { 
-                break; 
+            else {
+                break;
             }
         }
-        return (int) ans; 
+        
+        return ans;
     }
 };
