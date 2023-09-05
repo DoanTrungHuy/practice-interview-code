26ad894a30a5c3dba254d55fac7e76d2a87a7f72
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-09-05 22:32:45 +0700

    Time: 2040 ms (5.01%), Space: 21 MB (13.95%) - LeetHub

--- 2842-count-k-subsequences-of-a-string-with-maximum-beauty/2842-count-k-subsequences-of-a-string-with-maximum-beauty.cpp
+++ 2842-count-k-subsequences-of-a-string-with-maximum-beauty/2842-count-k-subsequences-of-a-string-with-maximum-beauty.cpp
@@ -1,14 +1,14 @@
 using ll = long long;
+const ll MOD = 1e9 + 7;
 const int MN = 1e6 + 1;
 ll p[MN];
-const ll MOD = 1e9 + 7;
 
 class Solution {
 public:
     ll exp(ll a, ll b) {
         if (b == 0) {
             return 1;
-        }
+        }    
         ll res = exp(a, b / 2);
         res = (res * res) % MOD;
         if (b & 1) {
@@ -16,56 +16,58 @@ public:
         }
         return res;
     }
-
+    
     ll nCr(ll n, ll r) {
         ll res = p[n];
         res = (res * exp(p[n - r], MOD - 2)) % MOD;
         res = (res * exp(p[r], MOD - 2)) % MOD;
         return res;
     }
-
+    
     int countKSubsequencesWithMaxBeauty(string s, int k) {
         unordered_map<char, ll> f;
-
-        for (char c : s) {
+        
+        for (const char c : s) {
             f[c]++;
         }
-
-        if (k > f.size()) {
+        
+        if (k > (int)f.size()) {
             return 0;
         }
-
-        priority_queue<ll> pq;
-
+        
+        priority_queue<int> pq;
+        
         for (const auto [character, freq] : f) {
             pq.push(freq);
         }
-
-        p[0] = 1LL;
-
+        
+        p[0] = 1;
+        
         for (int i = 1; i < MN; ++i) {
             p[i] = (p[i - 1] * i) % MOD;
         }
-
+        
         ll ans = 1LL;
-
+        
         while (k > 0) {
-            ll num_way = pq.top();
-            int cnt_num_of_freq = 0;
+            int num_way = pq.top();
+            ll num_freq = 0;
+            
             while (!pq.empty() and pq.top() == num_way) {
-                cnt_num_of_freq++;
+                num_freq++;
                 pq.pop();
             }
-            if (k >= cnt_num_of_freq) {
-                ans = (ans * exp(num_way, cnt_num_of_freq)) % MOD;
-                k -= cnt_num_of_freq;
+            
+            if (k >= num_freq) {
+                ans = (ans * exp(num_way, num_freq)) % MOD;
+                k -= num_freq;
             }
             else {
-                ans = (ans * ((nCr(cnt_num_of_freq, k) * exp(num_way, k)) % MOD)) % MOD;
+                ans = (((ans * nCr(num_freq, k)) % MOD) * exp(num_way, k)) % MOD;
                 break;
             }
         }
-
-        return (int) ans;
+        
+        return ans;
     }
 };
