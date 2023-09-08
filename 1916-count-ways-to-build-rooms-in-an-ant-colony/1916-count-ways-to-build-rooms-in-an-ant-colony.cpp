91444505cd6fdcf235a5fadb4873e9afad3f7b5e
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-09-08 09:57:00 +0700

    Time: 491 ms (52.87%), Space: 194.9 MB (61.07%) - LeetHub

--- 1916-count-ways-to-build-rooms-in-an-ant-colony/1916-count-ways-to-build-rooms-in-an-ant-colony.cpp
+++ 1916-count-ways-to-build-rooms-in-an-ant-colony/1916-count-ways-to-build-rooms-in-an-ant-colony.cpp
@@ -22,10 +22,12 @@ public:
     }
     
     int dfs(int node) {
+        if (adj[node].size() == 0) {
+            return 1;
+        }
         for (int child : adj[node]) {
             dfs(child);
             sz[node] += sz[child];
-            dp[node] = (1LL*dp[node]*dp[child]) % MOD;
             dp[node] = (1LL*dp[node]*exp(fact[sz[child]], MOD - 2)) % MOD;
         }
         dp[node] = (1LL*dp[node]*fact[sz[node] - 1]) % MOD;
@@ -45,6 +47,11 @@ public:
             fact[i] = (fact[i - 1]*i) % MOD;
         }
         dp.resize(n, 1), sz.resize(n, 1);
-        return dfs(0);
+        dfs(0);
+        int ans = 1;
+        for (int i = 0; i < n; ++i) {
+            ans = (1LL*ans*dp[i]) % MOD;
+        }
+        return ans;
     }
 };
