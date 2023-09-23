9f697c544382288b853cea44ce3ac07007cfbc86
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-09-23 20:48:13 +0700

    Time: 118 ms (47.64%), Space: 23.1 MB (39.99%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1048-longest-string-chain/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1048-longest-string-chain.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int longestStrChain(vector<string>& words) {
+        sort(words.begin(), words.end(), [](const string a, const string b) {
+            return a.size() < b.size();
+        });
+        const int n = words.size();
+        int dp[n];
+        int ans = 0;
+        
+        for (int i = 0; i < n; ++i) {
+            dp[i] = 1;
+            for (int j = 0; j < i; ++j) {
+                int sz[2] = {(int)words[j].size(), (int)words[i].size()};
+                if (sz[0] + 1 != sz[1]) {
+                    continue;
+                }
+                int p = 0;
+                for (char c : words[i]) {
+                    if (words[j][p] == c) {
+                        p++;
+                    }
+                } 
+                if (p != sz[0]) {
+                    continue;
+                }
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+            ans = max(ans, dp[i]);
+        }
+        
+        return ans;
+    }
+};
