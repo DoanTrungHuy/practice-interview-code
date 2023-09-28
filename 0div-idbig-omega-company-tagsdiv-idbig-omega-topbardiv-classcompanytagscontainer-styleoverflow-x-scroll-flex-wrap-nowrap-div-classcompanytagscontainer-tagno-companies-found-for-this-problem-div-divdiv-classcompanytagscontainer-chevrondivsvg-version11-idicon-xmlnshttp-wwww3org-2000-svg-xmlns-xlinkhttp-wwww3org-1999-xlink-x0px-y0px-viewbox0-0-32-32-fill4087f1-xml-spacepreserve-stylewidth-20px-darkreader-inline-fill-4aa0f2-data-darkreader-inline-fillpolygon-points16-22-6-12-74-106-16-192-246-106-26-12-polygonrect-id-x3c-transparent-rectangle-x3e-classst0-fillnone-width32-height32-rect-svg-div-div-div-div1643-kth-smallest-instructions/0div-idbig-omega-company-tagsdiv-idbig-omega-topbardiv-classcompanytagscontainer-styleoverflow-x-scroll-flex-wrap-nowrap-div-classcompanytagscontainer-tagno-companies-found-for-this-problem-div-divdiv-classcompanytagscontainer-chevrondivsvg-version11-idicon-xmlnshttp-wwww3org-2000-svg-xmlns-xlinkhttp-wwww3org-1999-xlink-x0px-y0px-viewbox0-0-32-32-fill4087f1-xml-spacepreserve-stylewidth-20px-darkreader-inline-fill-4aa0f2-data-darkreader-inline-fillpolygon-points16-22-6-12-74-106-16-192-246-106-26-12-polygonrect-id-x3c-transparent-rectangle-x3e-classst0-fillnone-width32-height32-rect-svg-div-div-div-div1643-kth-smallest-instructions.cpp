f9b7a48820e2ac3615a44d8c66b396fbf2fe048c
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-09-28 15:43:54 +0700

    Time: 11 ms (8.75%), Space: 16.6 MB (12.08%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1643-kth-smallest-instructions/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1643-kth-smallest-instructions.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string kthSmallestPath(vector<int>& destination, int k) {
+        const int n = destination[0];
+        const int m = destination[1];
+        
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 1));
+        
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = m - 1; j >= 0; --j) {
+                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+            }
+        }
+        
+        string path = "";
+        int r = 0, c = 0;
+        
+        while (r < n or c < m) {
+            if (c < m and dp[r][c] >= k) {
+                path.push_back('H');
+                c++;
+            }
+            else {
+                if ((k -= dp[r][c]) > 0) {
+                    c--;
+                    path.pop_back();
+                }
+                path.push_back('V');
+                r++;
+            }
+        }
+        
+        return path;
+    }
+};
