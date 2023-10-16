58ea19338d50a0bf0c78444d47e855583d0fb2d2
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-10-16 08:37:49 +0700

    Time: 10 ms (86.12%), Space: 8.2 MB (78.53%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -0,0 +1,27 @@
+const int MOD = 1e9 + 7;
+int memo[501][1001];
+
+class Solution {
+public:
+    int dp(int i, int j, int steps, int n) {
+        if (i == steps) {
+            return j == 0;
+        }
+        if (memo[i][j + 500] != -1) {
+            return memo[i][j + 500];
+        }
+        int ans = dp(i + 1, j, steps, n) % MOD;
+        if (j - 1 >= 0) {
+            ans = (ans + dp(i + 1, j - 1, steps, n)) % MOD;
+        }
+        if (j + 1 < n) {
+            ans = (ans + dp(i + 1, j + 1, steps, n)) % MOD;
+        }
+        return memo[i][j + 500] = ans;
+    }
+    
+    int numWays(int steps, int arrLen) {
+        memset(memo, -1, sizeof(memo));
+        return dp(0, 0, steps, arrLen);
+    }
+};
