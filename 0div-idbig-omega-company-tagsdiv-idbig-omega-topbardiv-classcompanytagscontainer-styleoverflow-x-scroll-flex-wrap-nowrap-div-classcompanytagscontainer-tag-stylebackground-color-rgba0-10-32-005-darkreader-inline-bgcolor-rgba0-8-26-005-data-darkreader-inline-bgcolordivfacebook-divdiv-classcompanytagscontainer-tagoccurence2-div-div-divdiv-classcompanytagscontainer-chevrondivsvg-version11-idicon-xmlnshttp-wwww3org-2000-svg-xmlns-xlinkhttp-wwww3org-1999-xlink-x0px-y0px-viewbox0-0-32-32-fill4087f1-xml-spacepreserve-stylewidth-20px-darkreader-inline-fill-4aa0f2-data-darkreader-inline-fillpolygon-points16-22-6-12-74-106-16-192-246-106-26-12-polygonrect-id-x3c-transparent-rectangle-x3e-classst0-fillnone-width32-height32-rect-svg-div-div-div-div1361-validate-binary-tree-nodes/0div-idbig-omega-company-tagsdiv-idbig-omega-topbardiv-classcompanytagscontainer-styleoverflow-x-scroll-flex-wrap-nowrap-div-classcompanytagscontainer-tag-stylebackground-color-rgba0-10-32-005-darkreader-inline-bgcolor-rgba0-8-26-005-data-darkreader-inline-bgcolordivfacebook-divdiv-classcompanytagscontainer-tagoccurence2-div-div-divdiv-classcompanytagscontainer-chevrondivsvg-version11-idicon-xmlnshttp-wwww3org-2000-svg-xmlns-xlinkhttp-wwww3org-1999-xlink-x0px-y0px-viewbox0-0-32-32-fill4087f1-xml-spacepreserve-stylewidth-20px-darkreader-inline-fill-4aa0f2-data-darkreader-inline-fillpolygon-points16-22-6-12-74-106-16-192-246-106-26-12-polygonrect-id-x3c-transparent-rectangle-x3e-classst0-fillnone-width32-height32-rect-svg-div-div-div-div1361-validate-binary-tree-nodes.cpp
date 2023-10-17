95132900fcec423120448123f2034abdf861b88b
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-10-17 10:43:01 +0700

    Time: 71 ms (21.73%), Space: 43.8 MB (23.25%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1361-validate-binary-tree-nodes/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1361-validate-binary-tree-nodes.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
+        unordered_set<int> has_node;
+        for (int child : leftChild) {
+            if (child != -1) {
+                has_node.insert(child);
+            }
+        }
+        for (int child : rightChild) {
+            if (child != -1) {
+                has_node.insert(child);
+            }
+        }
+        if (has_node.size() == n) {
+            return false;
+        }
+        int root = 0;
+        for (int i = 0; i < n; ++i) {
+            if (!has_node.count(i)) {
+                root = i;
+                break;
+            }
+        }
+        queue<int> mq;
+        unordered_set<int> visited;
+        mq.push(root);
+        visited.insert(root);
+        
+        while (!mq.empty()) {
+            int node = mq.front();
+            mq.pop();
+            
+            int left_node = leftChild[node];
+            int right_node = rightChild[node];
+            
+            if (left_node != -1 and visited.count(left_node)) {
+                return false;
+            }
+            
+            if (right_node != -1 and visited.count(right_node)) {
+                return false;
+            }
+            
+            if (left_node != -1) {
+                mq.push(left_node);
+                visited.insert(left_node);
+            }
+            if (right_node != -1) {
+                mq.push(right_node);
+                visited.insert(right_node);
+            }
+        }
+        
+        return visited.size() == n;
+    }
+};
