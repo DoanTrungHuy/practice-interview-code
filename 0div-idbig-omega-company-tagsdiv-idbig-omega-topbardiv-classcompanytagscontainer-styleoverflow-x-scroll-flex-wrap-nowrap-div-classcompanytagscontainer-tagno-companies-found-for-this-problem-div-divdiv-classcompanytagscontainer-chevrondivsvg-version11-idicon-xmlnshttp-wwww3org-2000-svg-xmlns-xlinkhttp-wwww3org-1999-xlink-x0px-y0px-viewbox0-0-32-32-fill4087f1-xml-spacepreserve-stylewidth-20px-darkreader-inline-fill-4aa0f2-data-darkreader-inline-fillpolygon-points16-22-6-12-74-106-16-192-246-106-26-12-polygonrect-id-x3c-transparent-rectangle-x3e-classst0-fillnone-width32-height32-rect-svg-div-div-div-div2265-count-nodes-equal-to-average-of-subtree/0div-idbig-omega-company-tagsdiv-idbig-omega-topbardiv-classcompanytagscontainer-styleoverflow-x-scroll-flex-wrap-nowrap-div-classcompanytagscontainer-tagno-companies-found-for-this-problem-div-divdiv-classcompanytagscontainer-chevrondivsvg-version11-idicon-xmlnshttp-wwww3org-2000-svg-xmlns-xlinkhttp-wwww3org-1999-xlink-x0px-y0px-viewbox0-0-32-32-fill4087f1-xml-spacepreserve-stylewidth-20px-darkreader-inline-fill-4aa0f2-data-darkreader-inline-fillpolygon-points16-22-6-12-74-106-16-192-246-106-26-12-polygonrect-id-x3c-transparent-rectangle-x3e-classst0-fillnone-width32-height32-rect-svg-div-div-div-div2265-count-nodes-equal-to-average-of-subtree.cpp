19276bcf11a4a193f9b99bcdb5c38ad973f7aa19
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2023-11-02 13:02:02 +0700

    Time: 16 ms (28.40%), Space: 23.1 MB (6.41%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2265-count-nodes-equal-to-average-of-subtree/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+private:
+    unordered_map<TreeNode*, int> memo, cnt;
+    int ans = 0;
+    
+public:
+    void dp(TreeNode* root) {
+        if (!root) {
+            return;
+        }
+        dp(root->left);
+        dp(root->right);
+        memo[root] = root->val;
+        cnt[root] = 1;
+        if (root->left) {
+            memo[root] += memo[root->left];
+            cnt[root] += cnt[root->left];
+        }
+        if (root->right) {
+            memo[root] += memo[root->right];
+            cnt[root] += cnt[root->right];
+        }
+    }
+    
+    void dfs(TreeNode* root) {
+        if (!root) {
+            return;
+        }
+        if (cnt[root] != 0 and memo[root] / cnt[root] == root->val) {
+            ans++;
+        }
+        dfs(root->left);
+        dfs(root->right);
+    }
+    
+    int averageOfSubtree(TreeNode* root) {
+        dp(root);
+        dfs(root);
+        return ans;
+    }
+};
