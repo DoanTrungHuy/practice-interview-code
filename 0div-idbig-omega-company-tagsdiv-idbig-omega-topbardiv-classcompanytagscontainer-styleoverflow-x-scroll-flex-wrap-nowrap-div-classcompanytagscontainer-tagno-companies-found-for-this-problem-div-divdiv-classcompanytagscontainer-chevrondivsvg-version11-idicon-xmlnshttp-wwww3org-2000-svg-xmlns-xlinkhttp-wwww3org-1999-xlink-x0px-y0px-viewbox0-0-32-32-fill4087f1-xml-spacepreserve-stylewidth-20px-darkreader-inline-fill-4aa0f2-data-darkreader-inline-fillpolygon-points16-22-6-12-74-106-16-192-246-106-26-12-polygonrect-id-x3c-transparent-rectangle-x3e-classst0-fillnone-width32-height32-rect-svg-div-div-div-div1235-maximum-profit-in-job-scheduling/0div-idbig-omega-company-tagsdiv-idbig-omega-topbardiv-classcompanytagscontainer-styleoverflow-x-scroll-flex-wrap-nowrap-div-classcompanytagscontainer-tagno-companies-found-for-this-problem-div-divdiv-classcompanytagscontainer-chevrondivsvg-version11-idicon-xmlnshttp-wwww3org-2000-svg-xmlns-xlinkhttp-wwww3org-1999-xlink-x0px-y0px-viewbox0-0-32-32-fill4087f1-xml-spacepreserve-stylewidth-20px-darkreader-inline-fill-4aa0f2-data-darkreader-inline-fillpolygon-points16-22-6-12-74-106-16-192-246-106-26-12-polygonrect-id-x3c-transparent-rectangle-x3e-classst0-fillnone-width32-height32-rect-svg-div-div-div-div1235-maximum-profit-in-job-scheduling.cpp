001747d3a70a551ab47c04c3112426eebc95c68e
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-01-06 09:16:03 +0700

    Time: 489 ms (6.99%), Space: 318.7 MB (5.06%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1235-maximum-profit-in-job-scheduling/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1235-maximum-profit-in-job-scheduling.cpp
@@ -0,0 +1,72 @@
+class Solution {
+private:
+    vector<int> tree;
+    
+public:
+    void update(int node, int left, int right, int index, int value) {
+        if (index < left or index > right) {
+            return;
+        }
+        if (left == right) {
+            tree[node] = value;
+            return;
+        }
+        int mid = (left + right) / 2;
+        update(node*2, left, mid, index, value);
+        update(node*2 + 1, mid + 1, right, index, value);
+        tree[node] = max(tree[node*2], tree[node*2 + 1]);
+    }
+    
+    int get(int node, int left, int right, int q_left, int q_right) {
+        if (q_left > right or q_right < left) {
+            return 0;
+        }
+        if (q_left <= left and right <= q_right) {
+            return tree[node];
+        }
+        int mid = (left + right) / 2;
+        int left_node = get(node*2, left, mid, q_left, q_right);
+        int right_node = get(node*2 + 1, mid + 1, right, q_left, q_right);
+        return max(left_node, right_node);
+    }
+    
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        const int MAXN = 1e6;
+        tree.resize(MAXN*4, 0);
+        int n = startTime.size();
+        using tp = tuple<int, int, int>;
+        map<int, int> compress;
+        vector<tp> events;
+        
+        for (int i = 0; i < n; ++i) {
+            int s = startTime[i];
+            int e = endTime[i];
+            int w = profit[i];
+            compress[s] = 0;
+            compress[e] = 0;
+            events.push_back({s, e, w});
+        }
+        
+        int next_day = 0;
+        
+        for (const auto &[time, day] : compress) {
+            compress[time] = ++next_day;
+        }
+        
+        sort(events.begin(), events.end(), [&](tp a, tp b) {
+             return get<1>(a) < get<1>(b);
+        });
+        
+        int dp[MAXN] = {};
+        
+        for (int i = 0; i < n; ++i) {
+            auto [s, e, w] = events[i];
+            s = compress[s];
+            e = compress[e];
+            dp[e] = max(dp[e], get(1, 1, MAXN, 1, s) + w);
+            update(1, 1, MAXN, e, dp[e]);
+        }
+        
+        return get(1, 1, MAXN, 1, MAXN);
+    }
+};
