8547f8f4788f95bf547c3ff632a7f0d46fb6dfba
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-01-21 16:47:43 +0700

    Time: 130 ms (17.21%), Space: 49.9 MB (5.08%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div907-sum-of-subarray-minimums/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div907-sum-of-subarray-minimums.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div907-sum-of-subarray-minimums/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div907-sum-of-subarray-minimums.cpp
@@ -1,24 +1,86 @@
+const int MOD = 1e9 + 7;
+
 class Solution {
+private:
+    vector<int> tree;
+    vector<int> arr;
+    int n;
+    
 public:
+    void update(int node, int left, int right, int index) {
+        if (index < left or index > right) {
+            return;
+        }
+        if (left == right) {
+            tree[node] = index;
+            return;
+        }
+        int mid = (left + right) / 2;
+        update(node*2, left, mid, index);
+        update(node*2 + 1, mid + 1, right, index);
+        if (tree[node*2] == -1) {
+            tree[node] = tree[node*2 + 1];
+        }
+        else if (tree[node*2 + 1] == -1) {
+            tree[node] = tree[node*2];
+        }
+        else if (arr[tree[node*2]] < arr[tree[node*2 + 1]]) {
+            tree[node] = tree[node*2];
+        }
+        else {
+            tree[node] = tree[node*2 + 1];
+        }
+    }
+    
+    int get(int node, int left, int right, int q_left, int q_right) {
+        if (left > q_right or q_left > right) {
+            return -1;
+        }
+        if (q_left <= left and right <= q_right) {
+            return tree[node];
+        }
+        int mid = (left + right) / 2;
+        int left_node = get(node*2, left, mid, q_left, q_right);
+        int right_node = get(node*2 + 1, mid + 1, right, q_left, q_right);
+        if (left_node == -1) {
+            return right_node;
+        }
+        else if (right_node == -1) {
+            return left_node;
+        }
+        else if (arr[left_node] < arr[right_node]) {
+            return left_node;
+        }
+        return right_node;
+    }
+    
+    long long dad(int left, int right) {
+        if (left == right) {
+            return arr[left];
+        }
+        if (left > right or right < 0 or left >= n) {
+            return 0;
+        }
+        int idx = get(1, 0, n - 1, left, right);
+        if (idx == -1) {
+            return 0;
+        }
+        long long s = ((long long)(idx - left + 1) * (right - idx + 1)) % MOD;
+        (s *= arr[idx]) % MOD;
+        (s += dad(left, idx - 1)) % MOD;
+        (s += dad(idx + 1, right)) % MOD;
+        return s;
+    }
+    
     int sumSubarrayMins(vector<int>& arr) {
-        arr.insert(arr.begin(), 0);
-        stack<int> st;
-        st.push(0);
-        int n = arr.size();
-        vector<int> dp(n, 0);
-        const int mod = 1e9 + 7;
-        for (int i = 1; i < n; ++i) {
-            while (!st.empty() and arr[st.top()] > arr[i]) {
-                st.pop();
-            }
-            int j = st.top();
-            dp[i] = dp[j] + (i - j) * arr[i];
-            st.push(i);
+        this->arr = arr;
+        this->n = arr.size();
+        tree.resize((n + 1) * 4, -1);
+        
+        for (int i = 0; i < n; ++i) {
+            update(1, 0, n - 1, i);
         }
-        int res = 0;
-        for (int i = 1; i < n; ++i) {
-            res = (res + dp[i]) % mod;
-        }
-        return res;
+        
+        return (dad(0, n - 1) + MOD) % MOD;
     }
 };
