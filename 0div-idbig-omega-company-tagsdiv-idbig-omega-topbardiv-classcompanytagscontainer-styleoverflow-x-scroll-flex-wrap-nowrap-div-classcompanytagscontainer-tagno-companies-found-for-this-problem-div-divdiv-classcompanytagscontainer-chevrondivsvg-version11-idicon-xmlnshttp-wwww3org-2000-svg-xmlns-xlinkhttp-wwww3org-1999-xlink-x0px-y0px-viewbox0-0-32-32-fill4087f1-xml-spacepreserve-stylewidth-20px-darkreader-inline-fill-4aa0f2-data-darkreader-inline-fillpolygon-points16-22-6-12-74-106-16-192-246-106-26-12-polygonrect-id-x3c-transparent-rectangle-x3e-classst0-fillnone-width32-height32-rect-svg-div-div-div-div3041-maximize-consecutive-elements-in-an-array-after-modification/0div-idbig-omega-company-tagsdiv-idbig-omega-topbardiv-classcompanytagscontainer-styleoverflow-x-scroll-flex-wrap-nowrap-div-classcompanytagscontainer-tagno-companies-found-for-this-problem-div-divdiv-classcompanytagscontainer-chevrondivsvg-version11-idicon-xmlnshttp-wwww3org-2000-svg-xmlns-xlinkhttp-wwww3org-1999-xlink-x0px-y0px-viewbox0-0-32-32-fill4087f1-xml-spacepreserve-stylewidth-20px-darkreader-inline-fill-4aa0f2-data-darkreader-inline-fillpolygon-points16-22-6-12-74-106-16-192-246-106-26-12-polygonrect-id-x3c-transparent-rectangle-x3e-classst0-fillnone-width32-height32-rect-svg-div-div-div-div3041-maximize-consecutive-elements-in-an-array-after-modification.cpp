40bbc382a563485e9f5e0490050c2c7ab3a217c6
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-02-18 01:04:51 +0700

    Time: 2267 ms (20.00%), Space: 440.9 MB (20.00%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3041-maximize-consecutive-elements-in-an-array-after-modification/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3041-maximize-consecutive-elements-in-an-array-after-modification.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3041-maximize-consecutive-elements-in-an-array-after-modification/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3041-maximize-consecutive-elements-in-an-array-after-modification.cpp
@@ -9,33 +9,19 @@ public:
         if (i == n) {
             return 0;
         }
-        
         if (memo[i].count(prev)) {
             return memo[i][prev];
         }
-        
         int ans = 0;
-        // Case 1: when prev has not been increased:
-        // Then we just consider whether nums[i] is one unit larger than prev?
-        {
-            if (nums[i] == prev + 1) {
-                ans = max(ans, 1 + dp(i + 1, nums[i]));
-            }
+        if (nums[i] == prev + 1) {
+            ans = max(ans, 1 + dp(i + 1, nums[i]));
         }
-        
-        // Case 2: when I increase "prev", there will be two cases as follows:
-        // One is the case where nums[i] is greater than 1 unit.
-        // Second is the case where nums[i] + 1 can be equal to prev.
-        // For example, if [1, 2, 3], when increasing at position 0, we get (prev = 2, nums[i] = 2), so we will ignore it and stop counting.
-        {
-            if (nums[i] + 1 == prev + 1) {
+        if (nums[i] + 1 == prev + 1) {
             ans = max(ans, 1 + dp(i + 1, nums[i] + 1));
-            }
-            if (nums[i] + 1 == prev) {
-                ans = max(ans, dp(i + 1, nums[i] + 1));
-            }
         }
-        
+        if (nums[i] + 1 == prev) {
+            ans = max(ans, dp(i + 1, nums[i] + 1));
+        }
         return memo[i][prev] = ans;
     }
     
@@ -45,14 +31,10 @@ public:
         this->n = nums.size();
         int ans = 0;
         memo.resize(n);
-        
         for (int i = 0; i < n; ++i) {
             ans = max(ans, 1 + dp(i + 1, nums[i]));
-        }
-        for (int i = 0; i < n; ++i) {
             ans = max(ans, 1 + dp(i + 1, nums[i] + 1));
         }
-        
         return ans;
     }
 };
