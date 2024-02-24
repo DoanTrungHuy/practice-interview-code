7867f940dcebd531bd39da4e82b95ad369f2dc87
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-02-24 14:50:05 +0700

    Time: 2404 ms (5.33%), Space: 557.6 MB (5.33%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2092-find-all-people-with-secret/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2092-find-all-people-with-secret.cpp
@@ -0,0 +1,64 @@
+class Solution {
+private:
+    vector<int> parent;
+    
+public:
+    int find(int u) {
+        if (u == parent[u]) {
+            return u;
+        }
+        return parent[u] = find(parent[u]);
+    }
+    
+    bool _union(int u, int v) {
+        u = find(u);
+        v = find(v);
+        
+        if (u == v) {
+            return true;
+        }
+        
+        parent[u] = v;
+        
+        return false;
+    }
+    
+    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
+        parent.resize(n);
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+        sort(meetings.begin(), meetings.end(), [](vector<int> a, vector<int> b) {
+           return a[2] < b[2]; 
+        });
+        _union(0, firstPerson);
+        const int m = meetings.size();
+        map<int, vector<pair<int, int>>> um;
+        for (int i = 0; i < m; ++i) {
+            int u = meetings[i][0];
+            int v = meetings[i][1];
+            int t = meetings[i][2];
+            um[t].push_back({u, v});
+        }
+        for (auto [t, sav] : um) {
+            for (auto [u, v] : sav) {
+                _union(u, v);
+            }
+            for (auto [u, v] : sav) {
+                if (find(u) != find(0)) {
+                    parent[u] = u;
+                }
+                if (find(v) != find(0)) {
+                    parent[v] = v;
+                }
+            }
+        }
+        vector<int> ans;
+        for (int i = 0; i < n; ++i) {
+            if (find(i) == find(0)) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
