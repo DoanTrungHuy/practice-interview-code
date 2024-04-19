7db83fefe44aa6ee84da089a7ee0d468bcf36c37
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-04-19 16:32:12 +0700

    Time: 396 ms (27.41%), Space: 170.7 MB (24.40%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3113-find-the-number-of-subarrays-where-boundary-elements-are-maximum/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3113-find-the-number-of-subarrays-where-boundary-elements-are-maximum.cpp
@@ -0,0 +1,69 @@
+class Solution {
+private:
+    vector<int> tree;
+    
+public:
+    void update(int node, int left, int right, int index, int value) {
+        if (index < left or right < index) {
+            return;
+        }    
+        if (left == right) {
+            tree[node] = value;
+            return;
+        }
+        int mid = (left + right) / 2;
+        update(node*2, left, mid, index, value);
+        update(node*2 + 1, mid + 1, right, index, value);
+        tree[node] = max(tree[node*2], tree[node*2 + 1]);
+    }
+    
+    int get_max(int node, int left, int right, int q_left, int q_right) {
+        if (right < q_left or q_right < left) {
+            return 0;
+        }    
+        if (q_left <= left and right <= q_right) {
+            return tree[node];
+        }
+        int mid = (left + right) / 2;
+        int left_node = get_max(node*2, left, mid, q_left, q_right);
+        int right_node = get_max(node*2 + 1, mid + 1, right, q_left, q_right);
+        return max(left_node, right_node);
+    }
+    
+    long long numberOfSubarrays(vector<int>& nums) {
+        const int n = nums.size();
+        tree.resize(4*n);
+        unordered_map<int, vector<int>> um;
+        
+        for (int i = 0; i < n; ++i) {
+            update(1, 0, n - 1, i, nums[i]);
+            um[nums[i]].push_back(i);
+        }
+        
+        long long ans = 0;
+        
+        // 2 5 2 1 2 5 2
+        
+        for (auto &[num, vec_num] : um) {
+            long long cnt = 1;
+            int prev_idx = vec_num[0];
+            const int m = vec_num.size();
+            
+            for (int i = 1; i < m; ++i) {
+                if (get_max(1, 0, n - 1, prev_idx, vec_num[i]) == num) {
+                    cnt++;
+                }
+                else {
+                    ans += cnt * (cnt - 1) / 2;
+                    ans += cnt;
+                    cnt = 1;
+                    prev_idx = vec_num[i];
+                }
+            }
+            ans += cnt * (cnt - 1) / 2;
+            ans += cnt;
+        }
+        
+        return ans;
+    }
+};
