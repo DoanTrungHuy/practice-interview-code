9512a09bd5cc4fdd8ae8ace619de7d7646323931
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-05-08 23:42:21 +0700

    Time: 2113 ms (33.20%), Space: 0B (100.00%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1070-product-sales-analysis-iii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1070-product-sales-analysis-iii.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+
+WITH rank_table_sale AS 
+(
+    SELECT *, (RANK() OVER(PARTITION BY product_id ORDER BY year ASC)) AS rnk_year
+   FROM Sales 
+)
+
+SELECT 
+    product_id, year AS first_year, quantity, price
+FROM 
+    rank_table_sale
+WHERE 
+    rnk_year = 1;
