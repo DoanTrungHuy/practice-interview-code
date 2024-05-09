1aa03aa42d7569cff4085bd1c41f9f83143c1869
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-05-09 13:47:19 +0700

    Time: 2319 ms (80.31%), Space: 0B (100.00%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1907-count-salary-categories/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1907-count-salary-categories.sql
@@ -0,0 +1,32 @@
+SELECT
+    category,
+    SUM(accounts_count) AS accounts_count
+FROM 
+(
+    SELECT
+        CASE 
+            WHEN income < 20000 THEN 'Low Salary'
+            WHEN income BETWEEN 20000 AND 50000 THEN 'Average Salary'
+            ELSE 'High Salary'
+        END AS category,
+        COUNT(*) AS accounts_count
+    FROM 
+        Accounts 
+    GROUP BY category
+
+    UNION ALL
+    SELECT 
+        'Low Salary' AS category,
+        0 AS accounts_count
+    
+    UNION ALL
+    SELECT 
+        'Average Salary' AS category,
+        0 AS accounts_count
+    
+    UNION ALL
+    SELECT 
+        'High Salary' AS category,
+        0 AS accounts_count
+) AS grouped_categories
+GROUP BY category;
