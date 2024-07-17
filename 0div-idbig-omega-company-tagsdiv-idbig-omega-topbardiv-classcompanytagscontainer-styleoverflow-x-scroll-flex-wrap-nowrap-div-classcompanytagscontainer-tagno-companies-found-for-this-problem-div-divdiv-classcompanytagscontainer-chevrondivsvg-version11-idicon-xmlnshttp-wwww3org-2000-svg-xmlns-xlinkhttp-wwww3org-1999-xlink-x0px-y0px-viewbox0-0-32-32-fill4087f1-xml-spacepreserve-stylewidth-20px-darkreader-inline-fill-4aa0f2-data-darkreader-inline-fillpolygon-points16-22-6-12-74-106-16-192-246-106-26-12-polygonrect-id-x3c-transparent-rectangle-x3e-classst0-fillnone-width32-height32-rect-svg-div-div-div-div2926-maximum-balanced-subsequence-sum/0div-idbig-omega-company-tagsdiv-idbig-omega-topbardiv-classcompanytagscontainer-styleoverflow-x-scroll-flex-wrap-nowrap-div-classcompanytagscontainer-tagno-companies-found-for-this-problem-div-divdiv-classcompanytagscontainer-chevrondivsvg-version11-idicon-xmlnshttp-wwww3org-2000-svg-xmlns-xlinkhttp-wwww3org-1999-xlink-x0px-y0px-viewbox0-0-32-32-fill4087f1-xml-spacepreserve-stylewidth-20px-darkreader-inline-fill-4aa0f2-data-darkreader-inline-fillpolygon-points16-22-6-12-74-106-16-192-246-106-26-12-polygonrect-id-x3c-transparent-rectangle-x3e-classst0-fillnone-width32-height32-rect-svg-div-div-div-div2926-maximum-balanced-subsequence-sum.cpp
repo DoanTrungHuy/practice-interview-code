51a03ca2be370aae44ca88002d0e90e5043f082d
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-07-17 22:11:45 +0700

    Time: 533 ms (18.22%), Space: 120.1 MB (31.96%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2926-maximum-balanced-subsequence-sum/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2926-maximum-balanced-subsequence-sum.cpp
@@ -0,0 +1,74 @@
+template <class T>
+struct SegmentTree {
+    vector<T> tree;
+    
+    SegmentTree() {
+        
+    }
+    
+    SegmentTree(int n) {
+        tree.resize(4*(n + 1));
+    }
+    
+    void update(int node, int left, int right, int index, T value) {
+        if (index < left || index > right) {
+            return;
+        }
+        if (left == right) {
+            tree[node] = max(tree[node], value);
+            return;
+        }
+        int mid = (left + right) >> 1;
+        update(node*2, left, mid, index, value);
+        update(node*2 + 1, mid + 1, right, index, value);
+        tree[node] = max(tree[node*2], tree[node*2 + 1]);
+    }
+    
+    T get_max(int node, int left, int right, int q_left, int q_right) {
+        if (q_left > right || q_right < left) {
+            return 0;
+        }
+        if (q_left <= left && right <= q_right) {
+            return tree[node];
+        }
+        int mid = (left + right) >> 1;
+        T left_node = get_max(node*2, left, mid, q_left, q_right);
+        T right_node = get_max(node*2 + 1, mid + 1, right, q_left, q_right);
+        return max(left_node, right_node);
+    }
+};
+
+class Solution {
+public:
+    long long maxBalancedSubsequenceSum(vector<int>& nums) {
+        // nums[i] - nums[j] >= i - j 
+
+        // nums[i] - i >= nums[j] - j
+        
+        const int n = nums.size();
+        map<int, int> compress;
+        
+        for (int i = 0; i < n; ++i) {
+            compress[nums[i] - i] = 0;
+        }
+        
+        int times = 0;
+        
+        for (auto &[v, f] : compress) {
+            compress[v] = ++times;
+        }
+        
+        SegmentTree<long long> st(n);
+        
+        long long res = LLONG_MIN;
+        
+        for (int i = 0; i < n; ++i) {
+            int prior_loc = compress[nums[i] - i];
+            long long new_sum = 1LL*nums[i] + st.get_max(1, 1, times, 1, prior_loc);
+            res = max(res, new_sum);
+            st.update(1, 1, times, prior_loc, new_sum);
+        }
+        
+        return res;
+    }
+};
