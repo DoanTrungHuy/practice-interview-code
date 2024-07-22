5d7323bccc00eb14fe32c75b78363ce4fb7648c0
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-07-23 02:40:26 +0700

    Time: 436 ms (6.16%), Space: 173.3 MB (5.21%) - LeetHub

---
+++ 2334-subarray-with-elements-greater-than-varying-threshold/2334-subarray-with-elements-greater-than-varying-threshold.cpp
@@ -0,0 +1,60 @@
+class Solution {
+private:
+    vector<int> parent, size;
+    
+public:
+    int find(int u) {
+        if (u == parent[u]) {
+            return u;
+        }
+        return parent[u] = find(parent[u]); 
+    }
+    
+    void _union(int u, int v) {
+        u = find(u);
+        v = find(v);
+        
+        parent[u] = v;
+        size[v] += size[u];
+    }
+    
+    int validSubarraySize(vector<int>& nums, int threshold) {
+        // num > threshold / k
+        
+        const int n = nums.size();
+        parent.resize(n);
+        size.resize(n, 1);
+        
+        priority_queue<pair<int, int>> pq;
+        
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+            pq.push({nums[i], i});
+        }
+        
+        unordered_set<int> us;
+        
+        while (!pq.empty()) {
+            auto [num, i] = pq.top();
+            pq.pop();
+            
+            us.insert(i);
+            
+            if (us.count(i - 1)) {
+                _union(i, i - 1);
+            }
+            
+            if (us.count(i + 1)) {
+                _union(i, i + 1);
+            }
+            
+            int k = size[find(i)];
+            
+            if (num*k > threshold) {
+                return k;
+            }
+        }
+        
+        return -1;
+    }
+};
