d23303a4eab1bfe84d5197ab753b5073e9252aad
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-07-24 15:35:32 +0700

    Time: 127 ms (14.13%), Space: 97 MB (11.91%) - LeetHub

--- 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.cpp
+++ 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.cpp
@@ -1,24 +1,37 @@
+const int MN = 2e5;
+
+int memo[MN][3];
+
 class Solution {
 public:
+    int dp(vector<int> &a, vector<int> &b, int i, int state) {
+        if (i == (int)a.size()) {
+            return 0;
+        }
+        
+        if (memo[i][state] != -1) {
+            return memo[i][state];
+        }
+        
+        int ans = 0;
+        
+        if (state == 0) {
+            ans = max(a[i] + dp(a, b, i + 1, state), b[i] + dp(a, b, i + 1, state + 1));
+        }
+        else if (state == 1) {
+            ans = max(b[i] + dp(a, b, i + 1, state), a[i] + dp(a, b, i + 1, state + 1));
+        }
+        else {
+            ans = a[i] + dp(a, b, i + 1, state);
+        }
+        
+        return memo[i][state] = ans;
+    }
+    
     int maximumsSplicedArray(vector<int>& nums1, vector<int>& nums2) {
-        const int n = nums1.size();
-        int sum1 = 0, sum2 = 0;
-        
-        for (int i = 0; i < n; ++i) {
-            sum1 += nums1[i];
-            sum2 += nums2[i];
-        }
-        
-        int max1 = 0, max2 = 0;
-        int cur1 = 0, cur2 = 0;
-        
-        for (int i = 0; i < n; ++i) {
-            cur1 = max(0, cur1 + nums2[i] - nums1[i]);
-            cur2 = max(0, cur2 + nums1[i] - nums2[i]);
-            max1 = max(max1, cur1);
-            max2 = max(max2, cur2);
-        }
-        
-        return max({sum1, sum2, sum1 + max1, sum2 + max2});
+        memset(memo, -1, sizeof(memo));
+        int ans = dp(nums1, nums2, 0, 0);
+        memset(memo, -1, sizeof(memo));
+        return max(ans, dp(nums2, nums1, 0, 0));
     }
 };
