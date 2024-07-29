6baac54099704e28d0522fe9eec1d409bea5969d
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-07-30 03:44:48 +0700

    Time: 19 ms (87.26%), Space: 12.1 MB (45.87%) - LeetHub

--- 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     long long appealSum(string s) {
-        // a : 1
-        // ab, b : 2, 1
+        // a: 1
+        // ab, b: 2, 1
         // abb, bb, b:  2, 1, 1
         // abbc, bbc, bc, c:  3, 2, 2, 1
         
         const int n = s.size();
-        int last[26] = {};
+        int prev[26] = {};
         long long ans = 0;
-        int total_substr = 0;
         
         for (int i = 0; i < n; ++i) {
-            total_substr += i + 1 - last[s[i] - 'a'];
-            last[s[i] - 'a'] = i + 1;
-            ans += total_substr;
+            ans += (i + 1 - prev[s[i] - 'a']) * (n - (i + 1)) + (i + 1 - prev[s[i] - 'a']);
+            prev[s[i] - 'a'] = i + 1;
         }
         
         return ans;
