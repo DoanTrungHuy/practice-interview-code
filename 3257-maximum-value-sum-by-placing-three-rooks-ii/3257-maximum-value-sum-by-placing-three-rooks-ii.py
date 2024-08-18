aad2818c79053a61fa82bf35a6eea34567effcd9
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-08-18 12:40:44 +0700

    Time: 1224 ms (100.00%), Space: 42.6 MB (100.00%) - LeetHub

---
+++ 3257-maximum-value-sum-by-placing-three-rooks-ii/3257-maximum-value-sum-by-placing-three-rooks-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maximumValueSum(self, board: List[List[int]]) -> int:
+        m,n = len(board), len(board[0])
+        data = []
+        for r in board:
+            h = []
+            for i,v in enumerate(r):
+                if len(h)==5: heappushpop(h, (v,i))
+                else: heappush(h, (v,i))
+            data.append(h)
+        h = []
+        for i,r in enumerate(data):
+            for v,j in r:
+                if len(h)==25: heappushpop(h, (v, i, j))
+                else: heappush(h, (v,i,j))
+        # print(data)
+        # print(h)
+        res=-math.inf
+        for x,y,z in combinations(h, 3):
+            sa, sb = {x[1], y[1], z[1]}, {x[2], y[2], z[2]}
+            if len(sa)==3 and len(sb)==3:
+                res = max(res, x[0]+y[0]+z[0])
+        return res
