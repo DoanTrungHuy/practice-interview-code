5df503dc6c85f0c4f4792159ef4450d7a69cf2b6
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-08-18 13:24:14 +0700

    Time: 959 ms (100.00%), Space: 42.5 MB (100.00%) - LeetHub

--- 3257-maximum-value-sum-by-placing-three-rooks-ii/3257-maximum-value-sum-by-placing-three-rooks-ii.py
+++ 3257-maximum-value-sum-by-placing-three-rooks-ii/3257-maximum-value-sum-by-placing-three-rooks-ii.py
@@ -2,23 +2,29 @@ class Solution:
     def maximumValueSum(self, board: List[List[int]]) -> int:
         m,n = len(board), len(board[0])
         data = []
+        
         for r in board:
             h = []
             for i,v in enumerate(r):
-                if len(h)==3: heappushpop(h, (v,i))
-                else: heappush(h, (v,i))
+                if len(h) == 3: 
+                    heappushpop(h, (v,i))
+                else: 
+                    heappush(h, (v,i))
             data.append(h)
         
         h = []
         for i,r in enumerate(data):
             for v,j in r:
-                if len(h)==100: heappushpop(h, (v, i, j))
-                else: heappush(h, (v,i,j))
+                if len(h) == 12: 
+                    heappushpop(h, (v, i, j))
+                else: 
+                    heappush(h, (v,i,j))
                     
-        res=-math.inf
+        res = -math.inf
+        
         for x,y,z in combinations(h, 3):
             sa, sb = {x[1], y[1], z[1]}, {x[2], y[2], z[2]}
-            if len(sa)==3 and len(sb)==3:
-                res = max(res, x[0]+y[0]+z[0])
+            if len(sa) == 3 and len(sb) == 3:
+                res = max(res, x[0] + y[0] + z[0])
                 
         return res
