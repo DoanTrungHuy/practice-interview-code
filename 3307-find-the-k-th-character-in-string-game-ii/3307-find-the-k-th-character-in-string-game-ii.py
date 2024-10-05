db1fcd4d20e24c8069ae503c738a8c5e45c5d655
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-10-05 07:59:26 +0700

    Time: 39 ms (94.61%), Space: 16.6 MB (57.51%) - LeetHub

--- 3307-find-the-k-th-character-in-string-game-ii/3307-find-the-k-th-character-in-string-game-ii.py
+++ 3307-find-the-k-th-character-in-string-game-ii/3307-find-the-k-th-character-in-string-game-ii.py
@@ -2,15 +2,18 @@ class Solution:
     def kthCharacter(self, k: int, ops: List[int]) -> str:
         n = len(ops)
         ans = 'a'
+        real_size = (1 << n)
         
         for i in range(n - 1, -1, -1):
-            if k > (1 << i):
+            real_size //= 2
+            
+            if k > real_size:
                 if ops[i]:
                     if ans == 'z':
                         ans = 'a'
                     else:
                         ans = chr(ord(ans) + 1)
                         
-                k -= (1 << i)
+                k -= real_size
                 
         return ans
