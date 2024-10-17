6773e7e1721bccd4db3f5167d87302b179777572
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-10-17 22:03:25 +0700

    Time: 109 ms (95.86%), Space: 83.2 MB (63.83%) - LeetHub

--- 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.cpp
+++ 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.cpp
@@ -1,47 +1,63 @@
-const int oo = INT_MAX;
-using pi = pair<int, int>;
+using pii = pair<int, int>;
+const int MN = 101;
+const int INF = 1e9;
+int dist[MN];
 
 class Graph {
 private:
-    vector<pi> adj[105];
+    vector<pii> adj[105];
     int n;
+    
+    // adj[fromi] = {{toi, weighti}, {}}
     
 public:
     Graph(int n, vector<vector<int>>& edges) {
         this->n = n;
-        for(auto edge : edges){
-            int u = edge[0];
-            int v = edge[1];
-            int w = edge[2];
+        
+        for (vector<int> e : edges) {
+            int u = e[0];
+            int v = e[1];
+            int w = e[2];
             adj[u].push_back({v, w});
-        } 
+        }
     }
-
-    void addEdge(vector<int> edge) {
-        int u = edge[0];
-        int v = edge[1];
-        int w = edge[2];
+    
+    void addEdge(vector<int> e) {
+        int u = e[0];
+        int v = e[1];
+        int w = e[2];
         adj[u].push_back({v, w});
     }
-
+    
     int shortestPath(int node1, int node2) {
-        priority_queue<pi, vector<pi>, greater<pi>> pq;
-        vector<int> d(n + 1, oo);
-        d[node1] = 0;
-        pq.push({0, node1});
+        priority_queue<pii, vector<pii>, greater<pii>> pq;
         
-        while(!pq.empty()){
-            auto [dist, node] = pq.top();
+        for (int i = 0; i < MN; ++i) {
+            dist[i] = INF;
+        }
+        
+        pq.push({dist[node1] = 0, node1});
+        
+        while (pq.size()) {
+            auto [du, u] = pq.top();
             pq.pop();
-            for(auto [v, w] : adj[node]){
-                if(dist + w < d[v]){
-                    d[v] = dist + w;
-                    pq.push({d[v], v});
+            
+            if (du != dist[u]) {
+                continue;
+            }
+            
+            if (u == node2) {
+                return du;
+            }
+            
+            for (auto [v, w] : adj[u]) {
+                if (du + w < dist[v]) {
+                    pq.push({dist[v] = du + w, v});
                 }
             }
         }
         
-        return d[node2] == oo ? -1 : d[node2];
+        return -1;
     }
 };
 
