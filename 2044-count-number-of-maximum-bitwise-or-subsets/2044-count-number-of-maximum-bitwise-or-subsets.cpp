1e8a84c86971f0ac241d1fc6b3781ec77c8d84d7
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-10-18 14:31:39 +0700

    Time: 11 ms (78.65%), Space: 21 MB (18.11%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -7,20 +7,20 @@ public:
         for (int num : nums) {
             max_or |= num;
         }
+        
+        int memo[n + 1][max_or + 1];
+        memset(memo, -1, sizeof(memo));
 
-        function<void(int, int)> dfs = [&](int i, int current_or) {
+        function<int(int, int)> dp = [&](int i, int current_or) -> int {
             if (i == n) {
-                if (current_or == max_or) {
-                    ++ans;
-                }
-                return;
+                return current_or == max_or;
             }
-
-            dfs(i + 1, current_or | nums[i]);
-            dfs(i + 1, current_or);
+            if (memo[i][current_or] != -1) {
+                return memo[i][current_or];
+            }
+            return memo[i][current_or] = dp(i + 1, current_or | nums[i]) + dp(i + 1, current_or);
         };
 
-        dfs(0, 0);
-        return ans;
+        return dp(0, 0);
     }
 };
