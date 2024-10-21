090ebdab029dff6ccec3f3f3254ad6567b3151cb
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-10-21 19:18:06 +0700

    Time: 6 ms (100.00%), Space: 9.3 MB (100.00%) - LeetHub

--- 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,29 +1,49 @@
+struct TrieNode {
+    bool is_word;
+    TrieNode *child[26];
+    
+    TrieNode() {
+        is_word = false;
+        memset(child, 0, sizeof child);
+    }
+};
+
 class Solution {
 private:
-    int ans;
-    unordered_set<long long> us;
+    TrieNode *root;
+    int n;
     
 public:
-    void dfs(string &s, int p) {
-        const int n = s.size();
-        if (p == n) {
-            ans = max(ans, (int)us.size());
-            return;
+    int dfs(string &s, int i) {
+        if (i == n) {
+            return 0;
         }
-        long long hash = 0;
-        for (int i = p; i < n; ++i) {
-            hash = hash*10 + s[i];
-            if (!us.count(hash)) {
-                us.insert(hash);
-                dfs(s, i + 1);
-                us.erase(hash);
+        
+        int ans = 0;
+        TrieNode *curr = root;
+        
+        for (int j = i; j < n; ++j) {
+            int index = s[j] - 'a';
+            
+            if (!curr->child[index]) {
+                curr->child[index] = new TrieNode();
+            }
+            
+            curr = curr->child[index];
+            
+            if (!curr->is_word) {
+                curr->is_word = true;
+                ans = max(ans, 1 + dfs(s, j + 1));
+                curr->is_word = false;
             }
         }
+        
+        return ans;
     }
     
     int maxUniqueSplit(string s) {
-        ans = 0;
-        dfs(s, 0);
-        return ans;
+        root = new TrieNode();
+        this->n = s.size();
+        return dfs(s, 0);
     }
 };
