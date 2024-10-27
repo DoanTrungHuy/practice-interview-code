37178f2213a5578adbe755bff1c357f5f211deb2
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-10-27 11:06:47 +0700

    Time: 19 ms (36.36%), Space: 25.6 MB (54.55%) - LeetHub

---
+++ 3334-find-the-maximum-factor-score-of-array/3334-find-the-maximum-factor-score-of-array.cpp
@@ -0,0 +1,34 @@
+using ll = long long;
+
+class Solution {
+public:
+    ll maxScore(vector<int>& nums) {
+        const ll n = nums.size();
+        
+        auto lcm = [](ll a, ll b) -> ll {
+            return (a / gcd(a, b)) * b;
+        };
+        
+        ll ans = 0, tg = nums[0], tl = nums[0];
+
+        for (ll i = 1; i < n; ++i) {
+            tg = gcd(tg, nums[i]), tl = lcm(tl, nums[i]);
+        }
+
+        ans = max(ans, (ll)tg * tl);
+        
+        for (ll i = 0; i < n; ++i) {
+            ll cd = 0, cl = 1;
+            
+            for (ll j = 0; j < n; ++j) {
+                if (i != j) {
+                    cd = (cd == 0) ? nums[j] : gcd(cd, nums[j]), cl = lcm(cl, nums[j]);
+                }
+            }
+            
+            ans = max(ans, (ll)cd * cl);
+        }
+        
+        return ans;
+    }
+};
