11e3eb0b04d72adb7b01673b88f72a4660a46fbb
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-11-09 09:18:16 +0700

    Time: 0 ms (100.00%), Space: 8.6 MB (42.66%) - LeetHub

--- 3133-minimum-array-end/3133-minimum-array-end.cpp
+++ 3133-minimum-array-end/3133-minimum-array-end.cpp
@@ -1,12 +1,33 @@
 class Solution {
 public:
     long long minEnd(int n, int x) {
-        long long t = x;
+        long long ans = x;
+        n--;
         
-        while (--n) {
-            t = (t + 1) | x;
+        // 000
+        // 001
+        // 010
+        
+        // x: 100101
+        // n: 0101
+        
+        int j = 0;
+        
+        while (x) {
+            if ((x & 1) == 0) {
+                ans |= (1LL << j)*(n&1);
+                n >>= 1;
+            }
+            x >>= 1;
+            j++;
         }
         
-        return t;
+        while (n) {
+            ans |= (1LL << j)*(n&1);
+            n >>= 1;
+            j++;
+        }
+        
+        return ans;
     }
 };
