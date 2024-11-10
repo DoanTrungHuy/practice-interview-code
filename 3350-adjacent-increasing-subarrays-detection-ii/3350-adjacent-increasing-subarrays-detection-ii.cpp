c9de25136bf0045886afa7426252d857338b6331
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-11-10 11:43:51 +0700

    Time: 392 ms (8.33%), Space: 284 MB (8.33%) - LeetHub

---
+++ 3350-adjacent-increasing-subarrays-detection-ii/3350-adjacent-increasing-subarrays-detection-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int maxIncreasingSubarrays(vector<int>& nums) {
+        const int n = nums.size();
+        
+        auto f = [&](int k) {
+            vector<int> dp(n);
+            int cnt = 0;
+            
+            for (int i = 0; i < n; ++i) {
+                if (i && nums[i] > nums[i - 1]) {
+                    ++cnt;
+                } 
+                else {
+                    cnt = 1;
+                }
+                dp[i] = cnt;
+            }
+            
+            for (int i = 0; i < n - 2*k + 1; ++i) {
+                if (dp[i + k - 1] - dp[i] == dp[i + 2*k - 1] - dp[i + k] && dp[i + 2*k - 1] - dp[i + k] == k - 1) {
+                    return true;
+                }
+            }
+            
+            return false;
+        };
+        
+        int left = 1, right = n / 2;
+        int ans = 0;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (f(mid)) {
+                ans = mid;
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
+            }
+        }
+        
+        return ans;
+    }
+};
