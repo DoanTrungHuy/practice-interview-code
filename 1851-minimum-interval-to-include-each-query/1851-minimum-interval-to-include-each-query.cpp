fdc4669529425be5b6b0edbbc5e8b6021e73f252
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-11-15 21:51:16 +0700

    Time: 818 ms (6.91%), Space: 177.5 MB (10.96%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
@@ -1,42 +1,88 @@
-#define START 0
-#define END 1
-#define QUERY 2
+class SegmentTree {
+public:
+    vector<int> tree, lazy;
+    
+    SegmentTree(int n = 0) {
+        tree.resize(4*n, INT_MAX);
+        lazy.resize(4*n, INT_MAX);
+    }
+    
+    void down(int node, int left, int right) {
+        if (left > right || lazy[node] == 0) {
+            return;
+        }
+        tree[node] = min(tree[node], lazy[node]);
+        if (left != right) {
+            lazy[node*2] = min(lazy[node*2], lazy[node]);
+            lazy[node*2 + 1] = min(lazy[node*2 + 1], lazy[node]);
+        }
+    }
+    
+    void update(int node, int left, int right, int q_left, int q_right, int value) {
+        down(node, left, right);
+        if (q_left > right or left > q_right) {
+            return;
+        }
+        if (q_left <= left and right <= q_right) {
+            lazy[node] = min(lazy[node], value);
+            down(node, left, right);
+            return;
+        }
+        int mid = (left + right) / 2;
+        update(node*2, left, mid, q_left, q_right, value);
+        update(node*2 + 1, mid + 1, right, q_left, q_right, value);
+        tree[node] = min(tree[node*2], tree[node*2 + 1]);
+    }
+    
+    int get(int node, int left, int right, int q_left, int q_right) {
+        down(node, left, right);
+        if (q_left > right or left > q_right) {
+            return INT_MAX;
+        }
+        if (q_left <= left and right <= q_right) {
+            return tree[node];
+        }
+        int mid = (left + right) / 2;
+        int left_node = get(node*2, left, mid, q_left, q_right);
+        int right_node = get(node*2 + 1, mid + 1, right, q_left, q_right);
+        return min(left_node, right_node);
+    }
+};
 
 class Solution {
 public:
     vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
-        map<int, set<pair<int, int>>> line;
-        int n = intervals.size();
+        map<int, int> cp;
         
-        for (int i = 0; i < n; ++i) {
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            int len = end - start + 1;
-            line[start].insert({START, len});
-            line[end + 1].insert({END, len});
+        for (vector<int> iter : intervals) {
+            int l = iter[0];
+            int r = iter[1];
+            cp[l], cp[r];
         }
         
-        int m = queries.size();
-        
-        for (int i = 0; i < m; ++i) {
-            line[queries[i]].insert({QUERY, i});
+        for (int q : queries) {
+            cp[q];
         }
         
-        vector<int> ans(m, -1);
-        multiset<int> sizes;
+        int times = 0;
         
-        for (auto [_, times] : line) {
-            for (auto [choose, size_or_index] : times) {
-                if (choose == START) {
-                    sizes.insert(size_or_index);
-                }
-                else if (choose == END) {
-                    sizes.erase(sizes.find(size_or_index));
-                }
-                else if (choose == QUERY and !sizes.empty()) {
-                    ans[size_or_index] = *sizes.begin();
-                }
-            }
+        for (auto &[k, v] : cp) {
+            v = times++;
+        }
+        
+        SegmentTree st(times);
+        
+        for (vector<int> iter : intervals) {
+            int l = iter[0];
+            int r = iter[1];
+            st.update(1, 0, times, cp[l], cp[r], r - l + 1);
+        }
+        
+        vector<int> ans;
+        
+        for (int q : queries) {
+            int val = st.get(1, 0, times, cp[q], cp[q]);
+            ans.push_back(val == INT_MAX ? -1 : val);
         }
         
         return ans;
