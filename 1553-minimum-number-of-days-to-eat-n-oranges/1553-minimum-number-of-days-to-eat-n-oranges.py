be60acee7497bb7c70eb27e99919151d9fb1761d
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-11-19 04:06:05 +0700

    Time: 43 ms (20.24%), Space: 17.6 MB (18.05%) - LeetHub

--- 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
@@ -2,8 +2,20 @@ class Solution:
     def minDays(self, n: int) -> int:
         @lru_cache(None)
         def dp(n):
-            if n <= 1:
-                return n
-            return 1 + min(n%2 + dp(n//2), n%3 + dp(n//3))
+            if n == 0:
+                return 0
+            
+            ans = 10**9
+            
+            if n % 2 == 0:
+                ans = min(ans, 1 + dp(n - n / 2))
+            
+            if n % 3 == 0:
+                ans = min(ans, 1 + dp(n - 2*(n / 3)))
+                
+            if n % 2 != 0 or n % 3 != 0:
+                ans = min(ans, 1 + dp(n - 1))
+                
+            return ans
         
         return dp(n)
