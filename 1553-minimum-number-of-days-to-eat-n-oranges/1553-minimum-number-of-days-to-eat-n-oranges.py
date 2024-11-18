23d2f8734df7c4ea066d55da9bb8871ade714825
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2024-11-19 03:53:37 +0700

    Time: 7 ms (82.74%), Space: 17.4 MB (21.20%) - LeetHub

--- 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
@@ -4,12 +4,6 @@ class Solution:
         def dp(n):
             if n <= 1:
                 return n
-            
-            ans = 10**9
-            
-            ans = min(ans, 1 + n % 3 + dp(n // 3))
-            ans = min(ans, 1 + n % 2 + dp(n // 2))
-                
-            return ans
+            return 1 + min(n%2 + dp(n//2), n%3 + dp(n//3))
         
         return dp(n)
