7f9954bae63125d74e93e702bac1e23bd4ea20a7
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2025-02-26 07:01:35 +0700

    Time: 59 ms (5.37%), Space: 128.6 MB (9.8%) - LeetHub

--- 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
@@ -1,23 +1,32 @@
 class Solution {
+private:
+    vector<int> arr;
+
 public:
-    int numOfSubarrays(vector<int>& arr) {
-        int odd = 0, even = 1;
-        int res = 0;
-        int sum = 0;
-        const int mod = 1e9 + 7;
-        
-        for (int num : arr) {
-            sum += num;
-            if (sum & 1) {
-                res = (res + even) % mod;
-                odd++;
-            }
-            else {
-                res = (res + odd) % mod;
-                even++;
-            }
+    int memo[100002][2][2];
+    int MOD = 1e9 + 7;
+
+    int dp(int i, bool t, bool o) {
+        if (i == arr.size()) {
+            return o;
         }
-        
-        return res;
+        if (memo[i][t][o] != -1) {
+            return memo[i][t][o];
+        }
+        int including = dp(i + 1, 1, o ^ (arr[i] % 2));
+        int excluding = 0;
+        if (t) {
+            excluding = dp(arr.size(), 1, o);
+        }
+        else {
+            excluding = dp(i + 1, 0, 0);
+        }
+        return memo[i][t][o] = (including + excluding) % MOD;
+    }
+
+    int numOfSubarrays(vector<int>& arr) {
+        this->arr = arr;
+        memset(memo, -1, sizeof(memo));
+        return dp(0, 0, 0);
     }
 };
