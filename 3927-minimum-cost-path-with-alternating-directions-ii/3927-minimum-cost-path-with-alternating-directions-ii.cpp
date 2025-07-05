c14d13af2d40bd8734119078d76422a06021e8e7
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2025-07-06 06:52:18 +0700

    Time: 616 ms (57.89%), Space: 381.6 MB (26.32%) - LeetHub

--- 3927-minimum-cost-path-with-alternating-directions-ii/3927-minimum-cost-path-with-alternating-directions-ii.cpp
+++ 3927-minimum-cost-path-with-alternating-directions-ii/3927-minimum-cost-path-with-alternating-directions-ii.cpp
@@ -15,17 +15,17 @@ public:
         if (memo[i][j][state] != -1) {
             return memo[i][j][state];
         }
-        if (state == ODD) {
+        if (state == EVEN) {
             long long ans = 1e12;
             if (i + 1 < m) {
-                ans = min(ans, dp(i + 1, j, EVEN) + (i + 1)*(j + 1));
+                ans = min(ans, dp(i + 1, j, ODD) + (i + 1)*(j + 1));
             }
             if (j + 1 < n) {
-                ans = min(ans, dp(i, j + 1, EVEN) + (i + 1)*(j + 1));
+                ans = min(ans, dp(i, j + 1, ODD) + (i + 1)*(j + 1));
             }
             return memo[i][j][state] = ans;
         }
-        long long ans = dp(i, j, ODD) + waitCost[i][j];
+        long long ans = dp(i, j, EVEN) + waitCost[i][j];
         return memo[i][j][state] = ans;
     }
 
@@ -34,6 +34,6 @@ public:
         this->m = m;
         this->n = n;
         this->waitCost = waitCost;
-        return dp(0, 0, ODD);
+        return dp(0, 0, EVEN);
     }
 };
