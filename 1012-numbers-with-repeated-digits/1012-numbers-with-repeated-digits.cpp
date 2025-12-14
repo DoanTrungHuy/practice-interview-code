31c9c0dcd847bf3c99c61f392902ec2e5565cbcf
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2025-12-14 17:50:08 +0700

    Time: 0 ms (100%), Space: 8.3 MB (52.61%) - LeetHub

--- 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.cpp
+++ 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.cpp
@@ -1,31 +1,48 @@
+// string tmp;
+int memo[11][2][2][1025];
+
 class Solution {
-public:
-    int dp[11][2][2][1 << 10];
+private: 
+    string s;
     int sz;
-    string nums;
-    
-    int dfs(int i, bool tight, bool hasRep, int mask) {
+
+public:
+    int dp(int i, int is_zero_at_head, int state, int mask) {
         if (i == sz) {
-            return hasRep;
+            return mask != 0;
         }
-        if (dp[i][tight][hasRep][mask] != -1) {
-            return dp[i][tight][hasRep][mask];
+        if (memo[i][is_zero_at_head][state][mask] != -1) {
+            return memo[i][is_zero_at_head][state][mask];
         }
-        int limit = tight ? nums[i] - '0' : 9;
+        int limit_number = state ? (s[i] - '0') : 9;
         int ans = 0;
-        for (int d = 0; d <= limit; ++d) {
-            bool newTight = tight and d == nums[i] - '0';
-            bool newRep = hasRep or mask & (1 << d); 
-            int newMask = d == 0 and mask == 0 ? mask : mask | (1 << d);
-            ans += dfs(i + 1, newTight, newRep, newMask);
+        for (int d = 0; d <= limit_number; ++d) {
+            if ((1 << d) & mask) {
+                continue;
+            }
+            int new_state = state && d == limit_number;
+            int new_bitmask = mask;
+            if (!is_zero_at_head || (d != 0)) {
+                new_bitmask = mask | (1 << d);
+            }
+            int new_is_zero_at_head = is_zero_at_head && d == 0;
+            // tmp += (d + '0');
+            ans += dp(i + 1, new_is_zero_at_head, new_state, new_bitmask);
+            // tmp.pop_back();
         }
-        return dp[i][tight][hasRep][mask] = ans;
+        return memo[i][is_zero_at_head][state][mask] = ans;
     }
-    
+
     int numDupDigitsAtMostN(int n) {
-        memset(dp, -1, sizeof(dp));
-        nums = to_string(n);
-        sz = nums.size();
-        return dfs(0, true, false, 0);
+        // dp(i, is_zero_at_head, state, bitmask)
+        // Coi như là xuất hiện ít nhất 2 lần
+        // Phủ định lại thì xuất hiện nhiều nhất 1 lần
+        // 2 <= number, 2 > number -> 1 >= number >= 0
+        // sử dụng bitmask
+        s = to_string(n);
+        sz = s.size();
+        memset(memo, -1, sizeof(memo));
+
+        return n - dp(0, 1, 1, 0);
     }
 };
