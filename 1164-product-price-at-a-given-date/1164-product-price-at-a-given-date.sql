53a56b6b55f2ff32bcd85411f2500f1299e7d9cb
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2026-01-28 12:19:17 +0700

    Time: 503 ms (42.23%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,20 +1,18 @@
 # Write your MySQL query statement below
 
-WITH cte AS (
-    SELECT *
-    FROM (
-        SELECT product_id, new_price AS price, RANK() OVER(PARTITION BY product_id ORDER BY change_date DESC) AS rnk_date
-        FROM Products
-        WHERE DATEDIFF("2019-08-16", change_date) >= 0
-    ) AS rank_products
-    WHERE rnk_date = 1
+WITH Fil AS (
+    SELECT product_id, MAX(change_date) AS cd FROM Products WHERE DATEDIFF(change_date, '2019-08-16') <= 0
+    GROUP BY product_id
 )
 
-SELECT p2.product_id, 
-(CASE WHEN p1.product_id IS NULL THEN 10 ELSE p1.price END) AS price
-FROM cte p1
-RIGHT JOIN (
-    SELECT product_id, 10 FROM Products
-    GROUP BY product_id
-) AS p2
-ON p1.product_id = p2.product_id
+SELECT product_id,
+CASE 
+WHEN (product_id, change_date) in (SELECT * FROM Fil) THEN new_price
+ELSE 10 END AS price
+FROM Products
+WHERE (product_id, change_date) in (SELECT * FROM Fil)
+UNION
+SELECT product_id, 
+10 AS price
+FROM Products AS p
+WHERE p.product_id not in (SELECT f.product_id FROM Fil as f)
