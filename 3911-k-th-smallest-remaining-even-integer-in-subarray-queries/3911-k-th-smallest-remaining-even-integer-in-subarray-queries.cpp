6b48ebe6b57e468261409e34c64796a999d7c1f0
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2026-04-29 01:10:12 +0700

    Time: 811 ms (12.1%), Space: 284.5 MB (24.21%) - LeetHub

--- 3911-k-th-smallest-remaining-even-integer-in-subarray-queries/3911-k-th-smallest-remaining-even-integer-in-subarray-queries.cpp
+++ 3911-k-th-smallest-remaining-even-integer-in-subarray-queries/3911-k-th-smallest-remaining-even-integer-in-subarray-queries.cpp
@@ -1,12 +1,31 @@
 class Solution {
 public:
-    vector<int> kthRemainingInteger(vector<int>& nums, vector<vector<int>>& queries) {
-        // 2, 4, 6, 8, 10, 12, 14, 16
-        //       --------
-        // TH1 (k < l): 2*k
-        // TH2 (k > r): (k + count_del)*2
-        // Th3 (l <= k <= r): (k + count_del_left)*2
+    int calc_gx(vector<int> &nums, vector<int> &pref, int l, int r, int x) {
+        const int n = nums.size();
+        int left = l;
+        int right = r;
+        int idx = -1;
 
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] <= x) {
+                idx = mid;
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        if (idx == -1) {
+            return 0;
+        }
+
+        return pref[idx + 1] - pref[l];
+    }
+
+    vector<int> kthRemainingInteger(vector<int>& nums, vector<vector<int>>& queries) {
         const int n = nums.size();
 
         vector<int> pref(n + 1);
@@ -22,24 +41,26 @@ public:
             int r = q[1];
             int k = q[2];
 
-            long long lo = 1, hi = 2e9 + 2e5 + 10;
-            while (lo < hi) {
-                long long mid = lo + (hi - lo) / 2;
-                // Số chẵn trong [2..mid]
-                long long total = mid / 2;
-                // Số chẵn bị xóa trong [nums[l]..nums[r]] mà <= mid
-                // dùng pref để đếm số chẵn trong nums[l..r] <= mid
-                // đếm chẵn trong nums[l..r] <= mid
-                long long pos = upper_bound(nums.begin()+l, nums.begin()+r+1, mid) - nums.begin();
-                
-                if (total - (pref[pos] - pref[l]) >= k) {
-                    hi = mid;
+            long long left = 1;
+            long long right = 1e18;
+            long long X = 0;
+
+            while (left <= right) {
+                long long mid = (left + right) / 2;
+
+                long long fx = mid / 2;
+                long long gx = calc_gx(nums, pref, l, r, mid);
+
+                if (k <= fx - gx) {
+                    X = mid;
+                    right = mid - 1;
                 }
                 else {
-                    lo = mid + 1;
+                    left = mid + 1;
                 }
             }
-            ans.push_back(lo % 2 == 0 ? lo : lo + 1);
+
+            ans.push_back(X & 1 ? X + 1 : X);
         }
 
         return ans;
