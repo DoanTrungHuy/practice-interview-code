6c6227b95362399bed0ae6184d06f7ddcf29d0c4
Huy <89284282+DoanTrungHuy@users.noreply.github.com>
2026-04-28 23:54:27 +0700

    Time: 541 ms (23.76%), Space: 284.6 MB (24.21%) - LeetHub

--- 3911-k-th-smallest-remaining-even-integer-in-subarray-queries/3911-k-th-smallest-remaining-even-integer-in-subarray-queries.cpp
+++ 3911-k-th-smallest-remaining-even-integer-in-subarray-queries/3911-k-th-smallest-remaining-even-integer-in-subarray-queries.cpp
@@ -24,37 +24,24 @@ public:
 
             int sum_del = pref[r + 1] - pref[l];
 
-            int left_bound_cnt_even = (nums[l] + 1) / 2;
-
-            if (left_bound_cnt_even > k) {
-                ans.push_back(2*k);
-                continue;
-            }
-
-            int right_bound_cnt_even = nums[r] / 2;
-
-            if (k > right_bound_cnt_even) {
-                ans.push_back(2*(k + sum_del));
-                continue;
-            }
-
-            int lo = 1, hi = 2e9;
+            long long lo = 1, hi = 2e9 + 2e5 + 10;
             while (lo < hi) {
-                int mid = lo + (hi - lo) / 2;
+                long long mid = lo + (hi - lo) / 2;
                 // Số chẵn trong [2..mid]
-                int total = mid / 2;
+                long long total = mid / 2;
                 // Số chẵn bị xóa trong [nums[l]..nums[r]] mà <= mid
                 // dùng pref để đếm số chẵn trong nums[l..r] <= mid
-                int left2 = l, right2 = r, deleted = 0;
                 // đếm chẵn trong nums[l..r] <= mid
-                int pos = upper_bound(nums.begin()+l, nums.begin()+r+1, mid) - nums.begin();
-                deleted = pref[pos] - pref[l];
+                long long pos = upper_bound(nums.begin()+l, nums.begin()+r+1, mid) - nums.begin();
                 
-                if (total - deleted >= k) hi = mid;
-                else lo = mid + 1;
+                if (total - (pref[pos] - pref[l]) >= k) {
+                    hi = mid;
+                }
+                else {
+                    lo = mid + 1;
+                }
             }
             ans.push_back(lo % 2 == 0 ? lo : lo + 1);
-
         }
 
         return ans;
